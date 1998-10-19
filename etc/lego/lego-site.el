e103401e809f4dc15e41962331a849a58a999a01
Thomas Kleymann <da+pg-tms@inf.ed.ac.uk>
1998-10-19 17:21:14 +0000

    support environmental variable PROOFGENERAL

---
+++ etc/lego/lego-site.el
@@ -0,0 +1,20 @@
+;;; proof-site.el  Site-specific Emacs support for LEGO
+;;; Copyright (C) 1998 LFCS Edinburgh
+;;; Author: Thomas Kleymann <T.Kleymann@ed.ac.uk>
+;;; Maintainer: lego@dcs.ed.ac.uk
+
+(let ((version (getenv "PROOFGENERAL")))
+  (cond ((string= version "")		;default
+	 (setq load-path
+	       (cons "/usr/local/share/elisp/script-management" load-path))
+	 (setq load-path
+	       (cons "/usr/local/share/elisp/script-management/lego" load-path))
+	 (load "lego"))
+	((string= version "ancient") 
+	 (setq load-path (cons "/usr/local/share/elisp/lego" load-path))
+	 (load "lego"))
+	((string= version "latest")
+	 (load-file "/usr/local/share/elisp/proofgeneral/generic/proof-site.el"))))
+	 
+
+	 
