c51e2febfd8d024a1bf8d16a0b19c63e612cd5c8
Thomas Kleymann <da+pg-tms@inf.ed.ac.uk>
1998-10-23 11:48:48 +0000

    no futher instructions in share-site-startup needed

--- etc/lego/lego-site.el
+++ etc/lego/lego-site.el
@@ -4,15 +4,18 @@
 ;;; Maintainer: lego@dcs.ed.ac.uk
 
 (let ((version (getenv "PROOFGENERAL")))
-  (cond ((string= version "")		;default
+  (cond ((not version)		;default
 	 (setq load-path
 	       (cons "/usr/local/share/elisp/script-management" load-path))
 	 (setq load-path
 	       (cons "/usr/local/share/elisp/script-management/lego" load-path))
-	 (load "lego"))
+	 (setq auto-mode-alist (cons '("\\.l$" . lego-mode) auto-mode-alist))
+	 (autoload 'lego-mode "lego" "Major mode for editing Lego proof scripts." t))
 	((string= version "ancient") 
 	 (setq load-path (cons "/usr/local/share/elisp/lego" load-path))
-	 (load "lego"))
+	 (setq auto-mode-alist (cons '("\\.l$" . lego-mode) auto-mode-alist))
+	 (autoload 'lego-mode "lego" "Major mode for editing Lego proof scripts." t)
+	 (autoload 'lego-shell "lego" "Inferior shell invoking lego." t))
 	((string= version "latest")
 	 (load-file "/usr/local/share/elisp/ProofGeneral/generic/proof-site.el"))))
 	 
