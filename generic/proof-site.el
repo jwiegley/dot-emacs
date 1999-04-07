e4f3c2ad6eccd68e82765eb45a5ede50a2738236
proofgen <>
1999-04-07 14:30:41 +0000

    Set version tag for new release.

--- generic/proof-site.el
+++ generic/proof-site.el
@@ -252,7 +252,7 @@ Note: to change proof assistant, you must start a new Emacs session.")
 
 ;; WARNING: do not edit below here 
 ;; (the next constant is set automatically)
-(defconst proof-version "Proof General, Version 2.1pre990222 released by da,tms. Email proofgen@dcs.ed.ac.uk."
+(defconst proof-version "Proof General, Version 2.1pre990407 released by da,tms. Email proofgen@dcs.ed.ac.uk."
  "Version string identifying Proof General release.")
 
 (provide 'proof-site)
