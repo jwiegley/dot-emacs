385e6f2309b0bb7e3eac68ec7e8da449e5fc77ef
David Aspinall <da@inf.ed.ac.uk>
1999-10-25 13:04:22 +0000

    Trivial tests for auto multiple files.

---
+++ etc/coq/multiple/c.v
@@ -0,0 +1,11 @@
+(* Silly tests for automatic auto file handling *)
+
+Goal (A,B:Prop)(and A B) -> (and B A).
+Intros A B H.
+Induction H.
+Apply conj.
+Assumption.
+Assumption.
+Save and_comms_c.
+
+
