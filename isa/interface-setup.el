79669423a345e65a8115323626f8ca66c44280a6
Makarius Wenzel <makarius@sketis.net>
2000-02-10 11:12:47 +0000

    setup proof-shell-pre-interrupt-hook for Poly/ML;

--- isa/interface-setup.el
+++ isa/interface-setup.el
@@ -5,3 +5,8 @@
 (customize-set-variable
  'isabelle-prog-name
  (concat (getenv "ISABELLE") " " (getenv "PROOFGENERAL_LOGIC")))
+
+(if (string-match "^polyml" (getenv "ML_SYSTEM"))
+    (customize-set-variable
+     'proof-shell-pre-interrupt-hook
+     (lambda () (proof-shell-insert "f" nil))))
