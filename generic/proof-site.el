4a2cfd1001b0f635d9c3ccdfcb46a1836aac981c
David Aspinall <da@inf.ed.ac.uk>
2000-04-07 17:29:15 +0000

    Set version tag for new release.

--- generic/proof-site.el
+++ generic/proof-site.el
@@ -276,7 +276,7 @@ Note: to change proof assistant, you must start a new Emacs session.")
 
 ;; WARNING: do not edit below here 
 ;; (the next constant is set automatically)
-(defconst proof-general-version "Proof General Version 3.2pre000404. Released by da."
+(defconst proof-general-version "Proof General Version 3.2pre000407. Released by da."
  "Version string identifying Proof General release.")
 
 (provide 'proof-site)
