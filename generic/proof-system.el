1544fa1ae2f85f090a6de797ecefca6575cf138f
David Aspinall <da@inf.ed.ac.uk>
2000-05-09 10:23:12 +0000

    Moved code to proof-menu.el

--- generic/proof-system.el
+++ generic/proof-system.el
@@ -14,49 +14,7 @@
 
 (require 'proof-config)
 
-
-(defmacro proof-customize-toggle (var)
-  "Make a function for toggling a boolean customize setting VAR.
-The toggle function uses customize-set-variable to change the variable."
-  `(lambda (arg)
-     ,(concat "Toggle " (symbol-name var) ". With ARG, turn on iff ARG>0.
-This function simply uses customize-set-variable to set the variable.
-It was constructed with the macro proof-customize-toggle.")
-     (interactive "P")
-     (customize-set-variable 
-      (quote ,var)
-      (if (null arg) (not ,var)
-	(> (prefix-numeric-value arg) 0)))))
-
-;; FIXME: combine this with above, and remove messing calls in
-;; proof-script.
-(defmacro proof-deftoggle (var)
-  "Define a function VAR-toggle for toggling a boolean customize setting VAR.
-The toggle function uses customize-set-variable to change the variable."
-  `(defun ,(intern (concat (symbol-name var) "-toggle")) (arg)
-     ,(concat "Toggle " (symbol-name var) ". With ARG, turn on iff ARG>0.
-This function simply uses customize-set-variable to set the variable.
-It was constructed with the macro proof-deftoggle.")
-     (interactive "P")
-     (customize-set-variable 
-      (quote ,var)
-      (if (null arg) (not ,var)
-	(> (prefix-numeric-value arg) 0)))))
-
-(defmacro proof-defshortcut (fn string &optional key)
-  "Define shortcut function FN to insert STRING, optional keydef KEY.
-This is intended for defining proof assistant specific functions.
-STRING is inserted using `proof-insert', which see.
-KEY is added onto proof-assistant map."
-  (if key
-      (eval
-       `(define-key proof-assistant-keymap ,key (quote ,fn))))
-  `(defun ,fn ()
-     "Shortcut command to insert a string."
-     (interactive)
-     (proof-insert ,string)))
-
-  
+;; Ooops!  Nothing here now.  Now in proof-menu.el
 
 
 ;; End of proof-system.el
