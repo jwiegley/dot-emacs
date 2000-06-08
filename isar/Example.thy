7c9810c7a262715e06a017ad2cedbdf62f4a7a14
Makarius Wenzel <makarius@sketis.net>
2000-06-08 19:53:12 +0000

    proper indentation;

--- isar/Example.thy
+++ isar/Example.thy
@@ -1,11 +1,11 @@
 (*  -*- isar -*-
 
-    Example proof document for Isabelle/Isar Proof General.
- 
-    $Id$
+      Example proof document for Isabelle/Isar Proof General.
+   
+      $Id$
 
-    The first line forces Isabelle/Isar Proof General, otherwise
-    you may get the theory mode of ordinary Isabelle Proof General
+      The first line forces Isabelle/Isar Proof General, otherwise
+      you may get the theory mode of ordinary Isabelle Proof General
 *)
 
 theory Example = Main:
@@ -13,11 +13,12 @@ theory Example = Main:
 theorem and_comms: "A & B --> B & A"
 proof
   assume "A & B"
-    thus "B & A"
-    proof
-      assume A B
-	show ?thesis 
-	..
-    qed
+  thus "B & A"
+  proof
+    assume A B
+    show ?thesis 
+      ..
+  qed
 qed
+
 end
