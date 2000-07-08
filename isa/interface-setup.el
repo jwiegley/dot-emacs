761d9aac830e435e59dcb97ae31a43fdcb5ac10f
Makarius Wenzel <makarius@sketis.net>
2000-07-08 14:28:21 +0000

    do not change isabelle-prog-name here; be less aggressive in changing x-symbol-enable;

--- isa/interface-setup.el
+++ isa/interface-setup.el
@@ -1,13 +1,11 @@
 ;;
 ;; $Id$
-;; 
+;;
 
-(customize-set-variable
- 'isabelle-prog-name
- (concat (getenv "ISABELLE") " " (getenv "PROOFGENERAL_LOGIC")))
-
-(customize-set-variable
- (if (equal (getenv "PROOFGENERAL_ASSISTANTS") "isa")
-     'isa-x-symbol-enable
-   'isar-x-symbol-enable)
- (equal (getenv "PROOFGENERAL_XSYMBOL") "true"))
+(let ((xsymbol (getenv "PROOFGENERAL_XSYMBOL")))
+  (if (and xsymbol (not (equal xsymbol "")))
+      (customize-set-variable
+       (if (equal (getenv "PROOFGENERAL_ASSISTANTS") "isa")
+           'isa-x-symbol-enable
+         'isar-x-symbol-enable)
+       (equal xsymbol "true"))))
