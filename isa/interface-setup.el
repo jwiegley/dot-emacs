234209f12a6b10b2057ebe991ffde9de3379a918
Makarius Wenzel <makarius@sketis.net>
2000-09-19 09:46:17 +0000

    improved xsymbol config: include info dir, only init for XEmacs;

--- isa/interface-setup.el
+++ isa/interface-setup.el
@@ -14,25 +14,28 @@
 ;;; X-Symbol
 ;;;
 
-(let ((xsymbol-home (getenv "XSYMBOL_HOME"))
-      (xsymbol (getenv "PROOFGENERAL_XSYMBOL"))
-      (enable-var
-       (if (equal (getenv "PROOFGENERAL_ASSISTANTS") "isa")
-           'isa-x-symbol-enable 'isar-x-symbol-enable)))
-  ;; setup the x-symbol package, if not already present
-  (if (and xsymbol-home
-	   (not (equal xsymbol-home ""))
-           (not (fboundp 'x-symbol-initialize))
-           (not (get 'x-symbol 'x-symbol-initialized)))
-      (progn
-        (load (expand-file-name "lisp/x-symbol/auto-autoloads" xsymbol-home))
-        (push (expand-file-name "lisp/x-symbol" xsymbol-home) load-path)
-        (if (boundp 'data-directory-list)
-            (push (expand-file-name "etc/" xsymbol-home) data-directory-list))
-        (x-symbol-initialize)))
-  ;; tell Proof General about -x option
-  (if (and xsymbol (not (equal xsymbol "")))
-      (customize-set-variable enable-var (equal xsymbol "true"))))
+(if (string-match "XEmacs" emacs-version)       ;current X-Symbol works with XEmacs only
+    (let ((xsymbol-home (getenv "XSYMBOL_HOME"))
+          (xsymbol (getenv "PROOFGENERAL_XSYMBOL"))
+          (enable-var
+           (if (equal (getenv "PROOFGENERAL_ASSISTANTS") "isa")
+               'isa-x-symbol-enable 'isar-x-symbol-enable)))
+      ;; setup the x-symbol package, if not already present
+      (if (and xsymbol-home
+               (not (equal xsymbol-home ""))
+               (not (fboundp 'x-symbol-initialize))
+               (not (get 'x-symbol 'x-symbol-initialized)))
+          (progn
+            (load (expand-file-name "lisp/x-symbol/auto-autoloads" xsymbol-home))
+            (push (expand-file-name "lisp/x-symbol" xsymbol-home) load-path)
+            (if (boundp 'data-directory-list)
+                (push (expand-file-name "etc/" xsymbol-home) data-directory-list))
+            (if (boundp 'Info-directory-list)
+                (push (expand-file-name "info/" xsymbol-home) Info-directory-list))
+            (x-symbol-initialize)))
+      ;; tell Proof General about -x option
+      (if (and xsymbol (not (equal xsymbol "")))
+          (customize-set-variable enable-var (equal xsymbol "true")))))
 
 
 ;;
