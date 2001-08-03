8abab39a51bf3a3adf1a6b889862478fd9e06a83
David Aspinall <da@inf.ed.ac.uk>
2001-08-03 13:07:27 +0000

    Fix link back to fileshow.php

--- html/fileshow.php
+++ html/fileshow.php
@@ -13,9 +13,9 @@
       substr($filename,0,1)=="~") {
      print "Sorry, can't show you that file!\n"; 
   } elseif (substr($filename,-3)==".el") {
-     elisp_markup($filename,"fileshow.html");
+     elisp_markup($filename,"fileshow.php");
   } else {
-     outline_markup($filename,"fileshow.html",$expanded);
+     outline_markup($filename,"fileshow.php",$expanded);
   }
   print "</pre>\n";
   print "<hr>";
