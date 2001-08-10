a825ef971fedfd36d83538b368f1104171200ca5
David Aspinall <da@inf.ed.ac.uk>
2001-08-10 12:55:55 +0000

    Bug test case, note workaround date

--- etc/isar/XEmacsSyntacticContextProb.thy
+++ etc/isar/XEmacsSyntacticContextProb.thy
@@ -7,7 +7,7 @@
    Bug doesn't occur in GNU Emacs (using imp of buffer-syntactic context
    provided in proof-compat.el), nor in XEmacs 21.4
 
-   Workaround added 
+   Workaround added Fri Aug 10 13:55:28 BST 2001
 *)
 
 theory XEmacsSyntacticContextProb = Main:
