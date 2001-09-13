5ab3d3c821df9dbe713fd2d4750b3968de3a3b74
David Aspinall <da@inf.ed.ac.uk>
2001-09-13 15:55:18 +0000

    Fix two more gaping holes letting people examine whole filesystem (also fixed in server anyway)

--- html/smallpage.php
+++ html/smallpage.php
@@ -1,6 +1,12 @@
 <?php
   require('functions.php3');
   small_header($title);
-  include($file);
+  if (substr($file,0,1)=="." or 
+      substr($file,0,1)=="/" or
+      substr($file,0,1)=="~") {
+     print "Sorry, can't show you that file!\n"; 
+  } else {
+    include($file);
+  }
   footer(); 
 ?>
