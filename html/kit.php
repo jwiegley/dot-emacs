954af91adb23aaae6203ccc228d20d7185445450
David Aspinall <da@inf.ed.ac.uk>
2001-09-13 15:53:41 +0000

    PHP in php, html and no extn link to php

--- html/kit.php
+++ html/kit.php
@@ -3,12 +3,15 @@
   small_header("Proof General Kit"); 
   ?>
 
+<p>
 The Proof General Kit project is in an early pre-experimental stage at
 the moment.  If you are interested in collaborating, or have ideas
 or suggestions to contribute, please send a note to
 <a href="mailto:kit@proofgeneral.org"><tt>kit@proofgeneral.org</tt></a>
+</p>
 
 <h3>Planning</h3>
+<p>
 Ideas for the future of Proof General are described in these papers:
 </p>
 <ul>
@@ -27,16 +30,18 @@ Ideas for the future of Proof General are described in these papers:
 <h3>Development</h3>
 <p>
 Not much has been started yet.
-</p>
-
+<br>
 But you can download the DTDs for PGIP and PGML, here:
+</p>
 <ul>
 <li><?php download_link("Kit/dtd/pgip.dtd") ?>
 </li>
 <li><?php download_link("Kit/dtd/pgml.dtd") ?>
 </li>
 </ul>
+<p>
 Comments and contributions welcomed!
+</p>
 
 <?php
    click_to_go_back();
