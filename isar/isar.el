990f07fb23220c8294c1090e6d44bd0c5a7dda18
Makarius Wenzel <makarius@sketis.net>
2001-12-05 15:34:20 +0000

    activate proof-shell-spill-output-regexp;

--- isar/isar.el
+++ isar/isar.el
@@ -206,7 +206,7 @@
    proof-shell-eager-annotation-start-length 1
    proof-shell-eager-annotation-start   "\360\\|\362"
    proof-shell-eager-annotation-end     "\361\\|\363"
-;   proof-shell-spill-output-regexp      "\375"
+   proof-shell-spill-output-regexp      "\375"
 
    ;; Some messages delimited by eager annotations
    proof-shell-clear-response-regexp    "Proof General, please clear the response buffer."
