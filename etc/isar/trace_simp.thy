bb2f58f761b01dc7346ec6aeb1acb776b8a63cb5
Makarius Wenzel <makarius@sketis.net>
2002-01-14 16:58:08 +0000

    tuned;

--- etc/isar/trace_simp.thy
+++ etc/isar/trace_simp.thy
@@ -1,8 +1,9 @@
 
 theory trace_simp = Main:
 
-(* this produces massive amount of simplifier trace, but terminates
-   eventually: *)
+text {*
+  this produces massive amount of simplifier trace, but terminates
+  eventually: *}
 
 ML {* set trace_simp *}
 ML {* reset quick_and_dirty *}
@@ -10,7 +11,7 @@ ML {* reset quick_and_dirty *}
 datatype ord = Zero | Succ ord | Limit "nat => ord"
 
 
-(* this one loops forever *)
+text {* this one loops forever *}
 
 lemma "ALL x. f x = g(f(g(x))) ==> f [] = f [] @ []"
   apply simp
