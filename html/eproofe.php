f33b1cb972d0d19708fc30f11d9722f353b7faa4
David Aspinall <da@inf.ed.ac.uk>
2002-05-03 13:34:55 +0000

    New files.

---
+++ html/eproofe.php
@@ -0,0 +1,30 @@
+<?php  
+  require('functions.php3');
+  small_header("Engineering Electronic Proof"); 
+  ?>
+
+<p> 
+Proof General, and particularly, the <a href="kit">Proof General
+Kit</a> is proposed as a vehicle for research into <i>engineering
+electronic proof<i>.  We want to investigate the
+maintenance, combination, and reuse of formal proof developments.
+</p>
+
+<h3>Planning</h3>
+<p>
+This project has not yet been started, and there are no public
+documents available yet.
+</p>
+<p>
+Some hints of our plans appear in the papers describing
+the <a href="kit">Proof General Kit</a>.
+</p>
+
+<p>
+Any <a href="mailto:da@dcs.ed.ac.uk">comments</a> are welcomed.
+</p>
+
+<?php
+   click_to_go_back();
+   footer();
+?>
