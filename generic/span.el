3be2b1fb7fa1e7a0df9369efb96c7e79df31de2c
David Aspinall <da@inf.ed.ac.uk>
2002-06-08 10:01:39 +0000

    Clean up span.el loading; make compat with bbdb.el in FSF

--- generic/span.el
+++ generic/span.el
@@ -5,16 +5,14 @@
 ;;
 ;; $Id$
 
-
+;;
 ;; Spans are our abstraction of extents/overlays.
-;; 
+;;
 (eval-and-compile
-  (cond
-   ((fboundp 'make-extent)    (require 'span-extent))
-   ((fboundp 'make-overlay)   (require 'span-overlay))
-   (t			       
-    (error 
-     "Your Emacs version is not compatible with Proof General, sorry."))))
-
+  (cond ((string-match "XEmacs" emacs-version)  
+	 (require 'span-extent))
+ 	(t 
+	 (require 'span-overlay))))
+  
 (provide 'span)
 ;; span.el ends here.
