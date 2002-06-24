72b2ab5a832f64c9bd93e6fb26c704029fa0c982
David Aspinall <da@inf.ed.ac.uk>
2002-06-24 09:06:17 +0000

    Add Emacs mode comments at top

--- isa/Example.thy
+++ isa/Example.thy
@@ -1,10 +1,16 @@
-(*
+(*  -*- isa -*- 
+
     Example theory file for Isabelle
 
     David Aspinall <da@dcs.ed.ac.uk>
 
     $Id$
 
+    The line at the top of this comment forces 
+    Proof General's classic Isabelle mode;
+    scripting takes place in .ML files.
+
+    NB: this is incompatible with PG/Isar.
 *)
 
 Example = Main
