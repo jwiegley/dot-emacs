09d7c8a5172a911efd5bce71799d805caf11055f
David Aspinall <da@inf.ed.ac.uk>
2002-08-12 09:21:43 +0000

    New files.

---
+++ html/eeproof.php
@@ -0,0 +1,32 @@
+<?php  
+  require('functions.php3');
+  small_header("Engineering Electronic Proof"); 
+  ?>
+
+<p>
+The <b>Engineering Electronic Proof</b> project is 
+a proposed continuation of the Proof General project,
+using Proof General as a vehicle to study and build
+new mechanisms for managing the development of
+electronic proof.
+</p>
+
+<h3>Details</h3>
+<p>
+More details will be posted here in due course.
+</p>
+<p>
+In the meantime, there is some related
+information on the <a href="kit">Proof General Kit</a>
+page about the next stages of development for Proof General.
+</p>
+<p>
+<h3>Collaborations</h3>
+Collaborations on this work are being sought.  If you are interested in collaborating, or have ideas or suggestions to contribute, please send a note to
+<a href="mailto:eeproof@proofgeneral.org"><tt>eeproof@proofgeneral.org</tt></a>
+</p>
+
+<?php
+   click_to_go_back();
+   footer();
+?>
