c078fce587c46ba73fc1ec146a05a9bdeb512669
David Aspinall <da@inf.ed.ac.uk>
2003-02-18 10:22:32 +0000

    Test with -- short comments.  Docs

--- isar/isar-mmm.el
+++ isar/isar-mmm.el
@@ -6,8 +6,11 @@
 ;;
 ;; $Id$
 ;; 
-;; TODO: more insertion commands might be nice.
-;; (Presently just C-c % t and C-c % M)
+;; TODO:
+;;  --- fontification for antiquotations has been lost, could
+;;      add that into LaTeX mode somehow.
+;;  --- more insertion commands might be nice.
+;;      (Presently just C-c % t and C-c % M)
 ;;
 
 (require 'mmm-auto)
@@ -15,7 +18,11 @@
 (defconst isar-start-latex-regexp 
   (concat
    "\\(" 
-   (proof-ids-to-regexp (list "text" "header" ".*section"))
+   (proof-ids-to-regexp 
+    (list "text" "header" ".*section"))
+   ;; Next one is nice but hammers font lock a bit too much
+   ;; if there are lots of -- {* short comments *}
+   ;; "\\|\-\-" ;; doesn't work with \\<--\\>
    "\\)[ \t]+{\\*"))
 
 (defconst isar-start-sml-regexp 
