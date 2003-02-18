63c2385e812fbe3c32071e2c2a01270f09b38e3b
David Aspinall <da@inf.ed.ac.uk>
2003-02-18 01:04:15 +0000

    New files.

---
+++ etc/isar/MultipleModes.thy
@@ -0,0 +1,24 @@
+header {* Some tests for \textbf{multiple modes!!} *}
+
+theory Example = Main:
+
+text {* Proper proof text -- \textit{naive version}. *}
+
+theorem and_comms: "A & B --> B & A"
+proof
+  assume "A & B"
+  then show "B & A"
+  proof
+    assume B and A
+    then
+   show ?thesis ..
+ qed
+qed
+
+ML_setup {*
+  fun fact 0 = 1 
+    | fact n = n * (fact (n-1))
+
+  val x = 7; 
+*}
+
