a6f7c0c9a31778993267c0ba4231d36ac2ae2a62
David Aspinall <da@inf.ed.ac.uk>
2003-02-19 12:09:38 +0000

    New files.

---
+++ etc/isar/Depends.thy
@@ -0,0 +1,16 @@
+(* In order to test the latest thm deps setup, consider this example: *)
+
+theory Depends = Main:
+
+  lemma I: "A ==> A" and K: "A ==> B ==> A" .
+
+(*
+This reports I, K depending on several things; for your internal
+dependency graph you may interpret this as each member of {I, K} depending
+on all the deps.
+
+        Markus
+*)
+
+end
+
