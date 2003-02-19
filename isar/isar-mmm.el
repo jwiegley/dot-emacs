58f90bc4befb95048be676e912a5e6446ab03027
David Aspinall <da@inf.ed.ac.uk>
2003-02-19 14:17:04 +0000

    Documentation.

--- isar/isar-mmm.el
+++ isar/isar-mmm.el
@@ -6,9 +6,14 @@
 ;;
 ;; $Id$
 ;; 
+;; Presently, we deal with several cases of {* text *}.
+;; It's not a good idea to do too much, since searching for the
+;; regions and fontifying them is slow.
+;; 
 ;; TODO:
 ;;  --- fontification for antiquotations has been lost, could
 ;;      add that into LaTeX mode somehow.
+;;  --- support for X-Symbols inside MMM mode?  (eek)
 ;;  --- more insertion commands might be nice.
 ;;      (Presently just C-c % t and C-c % M)
 ;;
@@ -19,10 +24,12 @@
   (concat
    "\\(" 
    (proof-ids-to-regexp 
-    (list "text" "header" ".*section"))
+    ;; Perhaps section is too much?  The fontification is nice but
+    ;; section headers are a bit short to use LaTeX mode in.
+    (list "text" "header" ".*section"))  
    ;; Next one is nice but hammers font lock a bit too much
    ;; if there are lots of -- {* short comments *}
-   ;; "\\|\-\-" ;; doesn't work with \\<--\\>
+   ;; "\\|\-\-" ;; NB: doesn't work with \\<--\\>
    "\\)[ \t]+{\\*"))
 
 (defconst isar-start-sml-regexp 
