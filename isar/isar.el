ebba1e38ee0f8a2b3465348db2e4295a6984c197
David Aspinall <da@inf.ed.ac.uk>
2003-02-22 17:36:41 +0000

    Move x-sym config to x-symbol-isar.el

--- isar/isar.el
+++ isar/isar.el
@@ -605,21 +605,4 @@ proof-shell-retract-files-regexp."
 	     x-symbol-isabelle-font-lock-keywords)))
   (proof-goals-config-done))
 
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; x-symbol support
-;;
-;; The following settings configure the generic PG package.
-;; The token language "Isabelle Symbols" is in file isa/x-symbol-isabelle.el
-;;
-
-(setq
- proof-xsym-activate-command
- (isar-markup-ml 
-  "print_mode := ([\"xsymbols\", \"symbols\"] @ ! print_mode)")
- proof-xsym-deactivate-command
- (isar-markup-ml 
-  "print_mode := (Library.gen_rems (op =) (! print_mode, [\"xsymbols\", \"symbols\"]))"))
-
 (provide 'isar)
