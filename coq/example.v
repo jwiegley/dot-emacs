45687f7f1ca127e881a80978f290b565ca629cf8
David Aspinall <da@inf.ed.ac.uk>
2003-10-05 15:57:35 +0000

    Rever to simplest example

--- coq/example.v
+++ coq/example.v
@@ -1,45 +1,14 @@
 (*
     Example proof script for Coq Proof General.
 
-    This is a legal script for coq 7.x, with
-    imbricated proofs definitions.
-
-    Replace Section by Module (>= coq-7.4).
- 
     $Id$
 *)
 
-Section sect.
-
 Goal (A,B:Prop)(A /\ B) -> (B /\ A).
  Intros A B H.
-Recursive Tactic Definition  bar := Idtac.
  Elim H.
  Intros.
-  Lemma foo: (A:Set)Set.
-  Intro A.
-  Exact A.  
-  Save.
- Split.  
+ Split.
  Assumption.
  Assumption.
 Save and_comms.
-End sect.
-
-Section newmod.
-Definition type1:=Set.
-
-Axiom A:False.
-Goal (n:nat)(S n)=n.
-Apply False_ind.
-Exact A.
-Save toto.
-End newmod.
-
-Extract Constant 
-
-Lemma Lem : (S O)=O.
-AutoRewrite [db].
-
-Hint Rewrite [toto] in db.
-AutoRewrite [db].
