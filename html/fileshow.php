b9caaa8e4b66817dbc66d0e79b567b3285869fea
David Aspinall <da@inf.ed.ac.uk>
2004-02-07 19:31:13 +0000

    Deleted file

--- html/fileshow.php
+++
@@ -1,8 +0,0 @@
-<?php
-  require('functions.php3');
-  require('elispmarkup.php3');
-  $filename=$HTTP_GET_VARS["file"];
-  $title=$HTTP_GET_VARS["title"];
-  $expanded=$HTTP_GET_VARS["expanded"];
-  fileshowmarkup($file,$title,$expanded);
-?>
--- html/index.php
+++
@@ -1,8 +0,0 @@
-<?php require('functions.php3'); ?>
-<?php  include('head.html'); ?>
-<?php
-    include('header.html');
-    include($WANTED . '.html' . $WANTEDFRAG);
-    footer();
-?>
-
