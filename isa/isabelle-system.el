cc4ba5ced2332a81b866a15876f2f5cabd21aace
David Aspinall <da@inf.ed.ac.uk>
2005-09-22 08:14:14 +0000

    Updated.

--- isa/isabelle-system.el
+++ isa/isabelle-system.el
@@ -82,7 +82,7 @@ with full path."
 	(progn
 	  (setq isatool-not-found t)
 	  (beep)
-	  (warn "Proof General: isatool command not found; some menus will be incomplete and Isabelle may not run correctly."))))
+	  (warn "Proof General: isatool command not found; some menus will be incomplete and Isabelle may not run correctly.  Please check your Isabelle installation."))))
   (file-executable-p isa-isatool-command))
 
 (defun isa-shell-command-to-string (command)
