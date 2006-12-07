bdbbe3e8716f313a5beaacd5cdd3e9d41528b9c4
Makarius Wenzel <makarius@sketis.net>
2006-12-07 19:53:14 +0000

    back again for isar;

---
+++ isar/interface-setup.el
@@ -0,0 +1,41 @@
+;; interface-setup.el Interface wrapper for Isabelle Proof General
+;;
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+;;
+;; Author: Markus Wenzel <wenzelm@in.tum.de>
+;;
+;; interface-setup.el,v 7.0 2002/08/29 09:14:03 da Exp
+;;
+
+;;
+;; X-Symbol
+;;
+
+(let ((xsymbol (getenv "PROOFGENERAL_XSYMBOL")))
+
+  ;; avoid confusing warning message
+  (if (not (boundp 'x-symbol-image-converter))     
+      (customize-set-variable 'x-symbol-image-converter nil))
+  
+  ;; tell Proof General about -x option
+  (if (and xsymbol (not (equal xsymbol "")))
+      (customize-set-variable 'isar-x-symbol-enable (equal xsymbol "true"))))
+
+
+;;
+;; Unicode
+;;
+
+(let ((unicode (getenv "PROOFGENERAL_UNICODE")))
+  (if (and unicode (not (equal unicode "")))
+      (customize-set-variable 'proof-shell-unicode (equal unicode "true"))))
+
+;;
+;; Proof General
+;;
+
+(if (not (featurep 'proof-site))
+    (load (concat (getenv "PROOFGENERAL_HOME") "/generic/proof-site.el")))
