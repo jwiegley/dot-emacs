bf5f65e7f091a955a67c58d8560fa5c5527a6d8d
David Aspinall <da@inf.ed.ac.uk>
2007-05-25 14:01:03 +0000

    New files.

---
+++ etc/coq/bug-114.v
@@ -0,0 +1,4 @@
+(* Trac #114:  identifiers starting with "fun" are wrongly coloured *)
+
+forall (funA B: Prop)     (* funA should be all magenta *)
+fun (forallA B: Prop)     (* forallA should be all magenta *)
