fdcd31d4ef474b4a7217aa65c239e6e271a35754
Makarius Wenzel <makarius@sketis.net>
2007-08-20 09:38:10 +0000

    proper use of ProofGeneral.sendback;

--- etc/isar/Sendback.thy
+++ etc/isar/Sendback.thy
@@ -1,17 +1,11 @@
-(* Demonstrate the use of literal command markup *)
+(* $Id$ *)
+
+header {* Demonstrate the use of literal command markup *}
 
 theory Sendback imports Main begin
 
-ML {* val pgasciiN = "PGASCII";
-fun special oct =
-  if print_mode_active pgasciiN then chr 1 ^ chr (ord (oct_char oct) - 167)
-  else oct_char oct; 
-fun sendback cmd = writeln ("Try this: \n   " ^ (special "376") ^ cmd ^ (special "377") ^ "\n\n")
-*}
-
 ML {*
-fun sendback2 cmd = (writeln "Try this: "; 
-		    ProofGeneral.sendback cmd [])
+fun sendback cmd = ProofGeneral.sendback "Try this:" [Pretty.str cmd]
 *}
 
 theorem and_comms: "A & B --> B & A"
