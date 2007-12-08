38072b9cd42819800a7316035fef6aca8a16c47e
David Aspinall <da@inf.ed.ac.uk>
2007-12-08 19:33:01 +0000

    New files.

---
+++ etc/coq/xemacs21528.v
@@ -0,0 +1,14 @@
+(* There are regexp problems with XEmacs 21.5.28 which break this example *)
+
+Module Type T.
+
+Parameter p1 : nat.
+Parameter p2 : nat.
+Parameter p3 : nat.
+Parameter p4 : nat.
+Parameter p5 : nat.
+Parameter p6 : nat.
+Parameter p7 : nat.
+Parameter p8 : nat.
+
+End T.
