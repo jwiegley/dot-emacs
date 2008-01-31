b9f40e7b69a7df1751ad132179b4bc28e30b43a5
David Aspinall <da@inf.ed.ac.uk>
2008-01-31 19:26:13 +0000

    Remove semi-colons.  Literal commands triggered in response buffer are now sent individually.  Should be compatible with original PBP behaviour which worked from goals buffer (and is anyway no longer used).

--- etc/isar/Sendback.thy
+++ etc/isar/Sendback.thy
@@ -11,8 +11,8 @@ fun sendback cmd = ProofGeneral.sendback "Try this:" [Pretty.str cmd]
 theorem and_comms: "A & B --> B & A"
 proof
   ML {* sendback "assume \"A & B\"" *}
-  ML {* sendback "then; show \"B & A\"" *}
-  ML {* sendback "proof; assume B and A; then" *}
-  ML {* sendback "show ?thesis; ..; qed" *}
+  ML {* sendback "then show \"B & A\"" *}
+  ML {* sendback "proof assume B and A then" *}
+  ML {* sendback "show ?thesis .. qed" *}
   ML {* sendback "qed" *}
 qed
