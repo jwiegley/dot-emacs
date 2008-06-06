faea8019ff496be6b5bf49f8257117fb1bddacef
Pierre Courtieu <courtieu@lri.fr>
2008-06-06 08:19:00 +0000

    Fixed example file for utf8.

--- coq/utf8.v
+++ coq/utf8.v
@@ -50,7 +50,7 @@ Notation "x ≥ y" := (ge x y) (at level 70, no associativity).
 
 (* test *)
 (*
-Goal ∀ x, True -> (∃ y , x ≥ y + 1) ∨ x ≤ y.
+Goal ∀ x, True -> (∃ y , x ≥ y + 1) ∨ x ≤ 0.
 *)
 
 (* Test for error message
