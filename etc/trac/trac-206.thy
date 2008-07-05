c626065a6eabefd66e9a850dffe71a884cadafb3
David Aspinall <da@inf.ed.ac.uk>
2008-07-05 16:47:14 +0000

    Trac regressions

---
+++ etc/trac/trac-206.thy
@@ -0,0 +1,14 @@
+theory Test imports Main begin
+
+(* The special markup (for terms etc.) is not processed in minibuffer
+messages. For example: *)
+
+ML_command {* warning (Syntax.string_of_typ @{context} @{typ 'a}) *}
+
+term "\<lambda> x. x"
+
+(* Here the decoration for 'a shows up as funny control characters,
+instead of proper font-lock colouring.  *)
+
+end
+
