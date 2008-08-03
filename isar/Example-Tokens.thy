fecfbc2d70ab83e3a8dff28229359dc8998fc2d7
David Aspinall <da@inf.ed.ac.uk>
2008-08-03 20:56:03 +0000

    Fix tokens

--- isar/Example-Tokens.thy
+++ isar/Example-Tokens.thy
@@ -12,7 +12,7 @@ text {* Proper proof text -- \<^bitalic>naive version\<^eitalic>. *}
 
 theorem and_comms: "\<phi> \<and> \<psi> \<longrightarrow> \<psi> \<and> \<phi>"
 proof
-   assume "\<phi> \<and> \<psi>"
+  assume "\<phi> \<and> \<psi>"
   then show "\<psi> \<and> \<phi>"
   proof
     assume \<psi> and \<phi>
@@ -22,7 +22,7 @@ qed
 
 text {* \<^bbold>Unstructured\<^ebold> proof script. *}
 
-theorem "\<phi>\<^isub>\<alpha> \<and> \<phi>\<^isub>\<beta> \<longrightarrow> (\<phi>\<^isub>\<beta>  ∧  \<phi>\<^isub>\<alpha>)"
+theorem "\<phi>\<^isub>\<alpha> \<and> \<phi>\<^isub>\<beta> \<longrightarrow> \<phi>\<^isub>\<beta> \<and> \<phi>\<^isub>\<alpha>"
   apply (rule impI)
   apply (erule conjE)
   apply (rule conjI)
