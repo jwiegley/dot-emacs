c440ded61788d0718065c494f7f19065da0e7e03
David Aspinall <da@inf.ed.ac.uk>
2009-08-20 11:00:44 +0000

    Remove spurious junk.

--- isar/Example.thy
+++ isar/Example.thy
@@ -18,38 +18,9 @@ proof
  qed
 qed
 
-(* but on the other hand, who knows? *)
-
-text {* Proper proof text -- \textit{advanced ve"rs"ion}. What do you think?  Who knows. *}
-theorem "B & A --> A & B"
-proof
-  assume "B & A"  -- "one of those kinds"
-  then obtain A and B ..
-  then show "A & B" ..
-qed 
-
-
-(* foo bar wiggle *)
-
-theorem "A & B --> B & A"
-proof
-  assume "A & B"
-  then obtain B and A ..
-  then show "B & A" .. 
-qed
-
-
-theorem "A & B --> B & A"
-proof
-  assume "A & B"
-  then obtain B and A ..
-  then show "B & A" .. 
-qed
-
-
 text {* Unstructured proof script. *}
 
-theorem  "A & B --> B & A" -- {* foo bar *}
+theorem  "A & B --> B & A"
   apply (rule impI)
   apply (erule conjE)
   apply (rule conjI)
