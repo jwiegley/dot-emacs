2e2334dcdb43bb645a5472e3ea5340e82e9d5308
David Aspinall <da@inf.ed.ac.uk>
2009-08-28 11:10:11 +0000

    Attempt to duplicate Dixon bug

---
+++ etc/isar/BigErrors.thy
@@ -0,0 +1,20 @@
+(* See http://proofgeneral.inf.ed.ac.uk/trac/ticket/274 *)
+
+theory BigErrors imports Pure 
+begin
+
+consts foo :: 'a;
+consts bar :: 'a;
+
+ML {* warning (cat_lines (List.tabulate (300,K "This is a big warning message"))); *}
+
+(* Attempt to get a big error with "error" fails, but we can use printing function
+   (see FaultyErrors.thy) *)
+
+ML {* Output.error_msg (cat_lines (List.tabulate (10000,K "This is a big error message"))); *}
+
+(* Note about FaultyErrors: the above generates an error
+   interactively but does *not* generate an error when 
+   required by another theory, see, BigErrorsNested *)
+
+end;
---
+++ etc/isar/BigErrorsNested.thy
@@ -0,0 +1,6 @@
+(* See http://proofgeneral.inf.ed.ac.uk/trac/ticket/274 *)
+
+theory BigErrorsNested imports BigErrors 
+begin
+
+end
