99d21e5c443ddbd1e773f1ef4baf2e9a6fe7b78c
David Aspinall <da@inf.ed.ac.uk>
2009-09-06 14:02:15 +0000

    Remove use-specials-for-fontify

--- generic/proof-config.el
+++ generic/proof-config.el
@@ -1351,19 +1351,10 @@ data triggered by `proof-shell-retract-files-regexp'."
   :type '(choice function (const nil))
   :group 'proof-shell)
 
-(defcustom pg-use-specials-for-fontify nil
-  "Flag indicating whether to strip annotations from output or not;
-\"annotations\" consist of special characters according to
-`pg-special-char-regexp'. If annotations are left in, they are made
-invisible and can be used to do syntax highlighting with font-lock."
-  :type 'boolean
-  :group 'proof-shell)
-
 (defcustom pg-special-char-regexp "[\200-\377]"
   "Regexp matching any \"special\" character sequence."
   :type 'string
   :group 'proof-shell)
-
 
 (defcustom proof-shell-set-elisp-variable-regexp nil
   "Matches output telling Proof General to set some variable.
