d0ad7ff9ec380737b42bbc20c66cb3293124e3c3
Makarius Wenzel <makarius@sketis.net>
2009-11-26 14:40:07 +0000

    additional menu entries;

--- isar/isar.el
+++ isar/isar.el
@@ -362,8 +362,9 @@ This is called when Proof General spots output matching
      (error "Aborted"))
   [(control p)])
 
-(proof-definvisible isar-cmd-refute	"refute" [r])
 (proof-definvisible isar-cmd-quickcheck "quickcheck" [(control q)])
+(proof-definvisible isar-cmd-nitpick "nitpick" [(control n)])
+(proof-definvisible isar-cmd-refute "refute" [r])
 (proof-definvisible isar-cmd-sledgehammer "sledgehammer" [(control s)])
 (proof-definvisible isar-cmd-atp-kill "atp_kill")
 (proof-definvisible isar-cmd-atp-info "atp_info")
@@ -374,8 +375,9 @@ This is called when Proof General spots output matching
    (list
     (cons "Commands"
 	  (list
-	   ["Refute"             isar-cmd-refute         t]
 	   ["Quickcheck"         isar-cmd-quickcheck     t]
+	   ["Nitpick"            isar-cmd-nitpick        t]
+	   ["Refute"             isar-cmd-refute         t]
 	   ["Sledgehammer"       isar-cmd-sledgehammer   t]
 	   ["Sledgehammer: kill" isar-cmd-atp-kill       t]
 	   ["Sledgehammer: info" isar-cmd-atp-info       t]
