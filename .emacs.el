242334f1be9fb630fd0e3250113fb060445044d2
John Wiegley <johnw@newartisans.com>
2010-05-27 12:31:51 -0600

    Added a function I use solely for creating proxies at work

--- .emacs.el
+++ .emacs.el
@@ -684,6 +684,22 @@
 
 (add-hook 'java-mode-hook 'my-java-mode-hook)
 
+(defun repair ()
+  (interactive)
+  (goto-char (point-min))
+  (re-search-forward "   _o\\([a-zA-Z0-9]+\\)\\.close();")
+  (let ((type (match-string 1)))
+    (while (re-search-forward "Exception;$" nil t)
+      (goto-char (match-end 0))
+      (delete-backward-char 1)
+      (let (method)
+	(save-excursion
+	  (re-search-backward " \\([a-zA-Z0-9]+\\)(")
+	  (setq method (match-string 1)))
+	(insert ?\n "   {")
+	(insert ?\n (format "      return _o%s.%s();" type method))
+	(insert ?\n "   }")))))
+
 ;;;_ * ledger
 
 (load "ldg-new")
