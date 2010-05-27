f151e8037736f70bf4224a6b1ff2ab6ecc1609d8
John Wiegley <johnw@newartisans.com>
2010-05-27 12:27:50 -0600

    abbreviate-file-name in Eshell prompt always

--- .emacs.el
+++ .emacs.el
@@ -117,6 +117,7 @@
  '(eshell-ls-initial-args "-h")
  '(eshell-modules-list (quote (eshell-alias eshell-basic eshell-cmpl eshell-dirs eshell-glob eshell-hist eshell-ls eshell-pred eshell-prompt eshell-rebind eshell-script eshell-term eshell-unix eshell-xtra)))
  '(eshell-prefer-to-shell t nil (eshell))
+ '(eshell-prompt-function (lambda nil (concat (abbreviate-file-name (eshell/pwd)) (if (= (user-uid) 0) " # " " $ "))))
  '(eshell-save-history-on-exit t)
  '(eshell-stringify-t nil)
  '(eshell-term-name "ansi")
