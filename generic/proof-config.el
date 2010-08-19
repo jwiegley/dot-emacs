e31355a6f51beb22034480a4e79be515d013a3f7
David Aspinall <da@inf.ed.ac.uk>
2010-08-19 10:16:20 +0000

    proof-shell-process-connection-type: Revert to long-lived previous default of t (for safety, no ill behaviour observed)

--- generic/proof-config.el
+++ generic/proof-config.el
@@ -1507,7 +1507,7 @@ This setting is used inside the function `proof-format-filename'."
   :type '(list (cons string string))
   :group 'proof-shell)
 
-(defcustom proof-shell-process-connection-type nil
+(defcustom proof-shell-process-connection-type t
   "The value of `process-connection-type' for the proof shell.
 Set non-nil for ptys, nil for pipes."
   :type 'boolean
