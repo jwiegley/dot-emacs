360e5ff834acc02c3cd90bc392f4f7c9bca9ce99
David Aspinall <da@inf.ed.ac.uk>
2010-08-30 22:45:52 +0000

    *** empty log message ***

---
+++ etc/trac/trac-346.v
@@ -0,0 +1,5 @@
+Open Scope ... (* is not colorized at all, *)
+
+Reserved Notation ... (* has only "Notation" colorized, *)
+
+Set Implicit Arguments. (* has only "Set" colorized, in a wrong way. *)
