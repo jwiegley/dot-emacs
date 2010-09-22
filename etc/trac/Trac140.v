50244404d9f435ab03397430d687102e19f69639
David Aspinall <da@inf.ed.ac.uk>
2010-09-22 09:32:48 +0000

    Test case for http://proofgeneral.inf.ed.ac.uk/trac/ticket/140

---
+++ etc/trac/Trac140.v
@@ -0,0 +1,15 @@
+Module Type T.
+
+Parameter p1 : nat.
+Parameter p2 : nat.
+Parameter p3 : nat.
+Parameter p4 : nat.
+Parameter p5 : nat.
+Parameter p6 : nat.
+Parameter p7 : nat.
+Parameter p8 : nat.
+Parameter p9 : nat.
+
+End T. 
+
+Time Print Module Type T. 
