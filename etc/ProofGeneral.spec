2b70dfdc722130c833fcedb26c7b1091d5500ac4
David Aspinall <da@inf.ed.ac.uk>
2010-10-10 17:05:42 +0000

    Set version tag for new release.

--- etc/ProofGeneral.spec
+++ etc/ProofGeneral.spec
@@ -1,6 +1,6 @@
 Summary:	Proof General, Emacs interface for Proof Assistants
 Name:		ProofGeneral
-Version:	4.0pre101004
+Version:	4.0pre101010
 Release:	1
 Group:		Text Editors/Integrated Development Environments (IDE)
 License:	GPL
--- generic/proof-site.el
+++ generic/proof-site.el
@@ -53,7 +53,7 @@
 
 (eval-and-compile
 ;; WARNING: do not edit next line (constant is edited in Makefile.devel)
-  (defconst proof-general-version "Proof General Version 4.0pre101004. Released by da."
+  (defconst proof-general-version "Proof General Version 4.0pre101010. Released by da."
     "Version string identifying Proof General release."))
 
 (defconst proof-general-short-version
