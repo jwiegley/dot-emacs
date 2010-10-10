f719e9c40651721189b31cf64cd10b91c5aea504
David Aspinall <da@inf.ed.ac.uk>
2010-10-10 22:59:07 +0000

    Set version tag for new release.

--- generic/proof-site.el
+++ generic/proof-site.el
@@ -53,7 +53,7 @@
 
 (eval-and-compile
 ;; WARNING: do not edit next line (constant is edited in Makefile.devel)
-  (defconst proof-general-version "Proof General Version 4.0pre101010. Released by da."
+  (defconst proof-general-version "Proof General Version 4.0. Released by da on Sun 10 Oct 2010."
     "Version string identifying Proof General release."))
 
 (defconst proof-general-short-version
