b2902c31425967e0f6a534ed1ba6ec2041c7d9f2
John Wiegley <johnw@newartisans.com>
2011-08-04 03:21:09 -0500

    Whitespace fix

--- .emacs.el
+++ .emacs.el
@@ -1073,17 +1073,17 @@ If the buffer is currently not visible, makes it sticky."
 (define-key mode-specific-map [? ] 'just-one-space)
 
 ;; inspired by Erik Naggum's `recursive-edit-with-single-window'
- (defmacro recursive-edit-preserving-window-config (body)
-   "*Return a command that enters a recursive edit after executing BODY.
+(defmacro recursive-edit-preserving-window-config (body)
+  "*Return a command that enters a recursive edit after executing BODY.
  Upon exiting the recursive edit (with\\[exit-recursive-edit] (exit)
  or \\[abort-recursive-edit] (abort)), restore window configuration
  in current frame."
-   `(lambda ()
-      "See the documentation for `recursive-edit-preserving-window-config'."
-      (interactive)
-      (save-window-excursion
-        ,body
-        (recursive-edit))))
+  `(lambda ()
+     "See the documentation for `recursive-edit-preserving-window-config'."
+     (interactive)
+     (save-window-excursion
+       ,body
+       (recursive-edit))))
 
 (define-key mode-specific-map [?0]
   (recursive-edit-preserving-window-config (delete-window)))
