6c12b47b3f098be5d0d0d9b04a30688a7cb10fa1
John Wiegley <johnw@newartisans.com>
2011-09-20 20:20:21 -0500

    Set erc-fools

--- settings.el
+++ settings.el
@@ -88,6 +88,7 @@
  '(erc-autoaway-mode t)
  '(erc-autojoin-channels-alist (quote (("localhost" "&bitlbee") ("freenode.net" "#ledger") ("irc.oftc.net" "#llvm"))))
  '(erc-autojoin-mode t)
+ '(erc-fools (quote ("JordiGH")))
  '(erc-generate-log-file-name-function (quote erc-generate-log-file-name-short))
  '(erc-header-line-format nil)
  '(erc-hide-list (quote ("JOIN" "NICK" "PART" "QUIT" "MODE")))
