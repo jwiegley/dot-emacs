a970fea5aa9949c16cf729fb436be5522b261f26
John Wiegley <johnw@newartisans.com>
2012-01-04 10:22:50 -0600

    Disable ido-visit-buffer advice

--- emacs.el
+++ emacs.el
@@ -541,15 +541,15 @@
 
 (diminish 'workgroups-mode)
 
-(defadvice ido-visit-buffer (before switch-to-buffers-workgroup
-                                    (buffer method &optional record)
-                                    activate)
-  "If a workgroup is showing BUFFER, switch to it first"
-  (wg-aif (wg-find-buffer (if (bufferp buffer)
-                              (buffer-name buffer)
-                            buffer))
-      (ignore-errors
-        (wg-switch-to-workgroup it))))
+;;(defadvice ido-visit-buffer (before switch-to-buffers-workgroup
+;;                                    (buffer method &optional record)
+;;                                    activate)
+;;  "If a workgroup is showing BUFFER, switch to it first"
+;;  (wg-aif (wg-find-buffer (if (bufferp buffer)
+;;                              (buffer-name buffer)
+;;                            buffer))
+;;      (ignore-errors
+;;        (wg-switch-to-workgroup it))))
 
 ;;;_ , yasnippet
 
