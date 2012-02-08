b0c6cae90829ba5b12a466716e4f3626e6cc70b5
David Aspinall <da@inf.ed.ac.uk>
2012-02-08 18:29:13 +0000

    Duplicate proof

--- hol-light/example.ml
+++ hol-light/example.ml
@@ -10,3 +10,10 @@ e CONJ_TAC;;
 e (ASM_SIMP_TAC[]);;
 e (ASM_SIMP_TAC[]);;
 let and_comms = top_thm();;
+
+g `A /\ B ==> B /\ A`;;
+e DISCH_TAC;;
+e CONJ_TAC;;
+e (ASM_SIMP_TAC[]);;
+e (ASM_SIMP_TAC[]);;
+let and_comms2 = top_thm();;
