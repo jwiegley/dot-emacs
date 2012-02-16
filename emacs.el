af9b9e13defe73aed7c6640d6984c90aabbe7767
John Wiegley <johnw@newartisans.com>
2012-02-15 20:40:08 -0600

    M-a-g does a w3m google search, M-a-w wikipedia

--- emacs.el
+++ emacs.el
@@ -2164,7 +2164,13 @@ Else, return \" \"."
 (define-key override-global-map [(meta ?v)] 'yank)
 (define-key global-map [(meta ?v)] 'yank)
 
+(defun wikipedia-query (term)
+  (interactive (list (read-string "Wikipedia search: " (word-at-point))))
+  (w3m-search "en.wikipedia" term))
+
 (define-key global-map [(alt meta ?f)] 'gnus-query)
+(define-key global-map [(alt meta ?g)] 'w3m-search)
+(define-key global-map [(alt meta ?w)] 'wikipedia-query)
 
 (define-key global-map [(control ?h) (control ?i)] 'info-lookup-symbol)
 
