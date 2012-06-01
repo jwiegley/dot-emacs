9a072c33c74e37036d46d7a83861372fee9c1ad7
John Wiegley <johnw@newartisans.com>
2012-06-01 04:45:54 -0500

    Use backup-each-save

--- emacs.el
+++ emacs.el
@@ -231,6 +231,26 @@
 (define-key ac-menu-map "\C-n" 'ac-next)
 (define-key ac-menu-map "\C-p" 'ac-previous)
 
+;;;_ , backup-each-save
+
+(require 'backup-each-save)
+
+(add-hook 'after-save-hook 'backup-each-save)
+
+(defun backup-each-save-filter (filename)
+  (let ((ignored-filenames
+         '("^/tmp" "semantic.cache$" "\\.emacs-places$"
+           "\\.recentf$" ".newsrc\\(\\.eld\\)?"))
+        (matched-ignored-filename nil))
+    (mapc
+     (lambda (x)
+       (when (string-match x filename)
+         (setq matched-ignored-filename t)))
+     ignored-filenames)
+    (not matched-ignored-filename)))
+
+(setq backup-each-save-filter-function 'backup-each-save-filter)
+
 ;;;_ , css-mode
 
 (add-to-list 'auto-mode-alist '("\\.css$" . css-mode))
