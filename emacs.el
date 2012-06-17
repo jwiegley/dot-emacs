78dfbeb99e5c9e20393c0465d76f14a820cf8dfd
John Wiegley <johnw@newartisans.com>
2012-06-17 11:41:52 -0500

    Added missing `list' in find-alternate-file-with-sudo

--- emacs.el
+++ emacs.el
@@ -241,8 +241,8 @@
 
 (defun find-alternate-file-with-sudo (filename)
   (interactive
-   (read-file-name "Find alternate file: " nil
-                   nil nil (concat "/sudo::" (buffer-file-name))))
+   (list (read-file-name "Find alternate file: " nil
+                         nil nil (concat "/sudo::" (buffer-file-name)))))
   (find-alternate-file filename))
 
 (bind-key "C-x C-v" 'find-alternate-file-with-sudo)
