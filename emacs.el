f39cb6f2f6a3cea62a7ba2064844509c8baa6c36
John Wiegley <johnw@newartisans.com>
2012-06-17 09:38:31 -0500

    Minor reformatting

--- emacs.el
+++ emacs.el
@@ -620,12 +620,11 @@
              el-get-update
              el-get-list-packages)
   :config
-  (progn
-    (defun el-get-read-status-file ()
-      (mapcar #'(lambda (entry)
-                  (cons (plist-get entry :symbol)
-                        `(status "installed" recipe ,entry)))
-              el-get-sources))))
+  (defun el-get-read-status-file ()
+    (mapcar #'(lambda (entry)
+                (cons (plist-get entry :symbol)
+                      `(status "installed" recipe ,entry)))
+            el-get-sources)))
 
 ;;;_ , abbrev
 
