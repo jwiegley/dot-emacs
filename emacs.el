de6a5aab1449a9209bfa88189b151f57aaaa52c1
John Wiegley <johnw@newartisans.com>
2012-06-19 00:48:56 -0500

    Added iflipb and dropped delsel

--- emacs.el
+++ emacs.el
@@ -208,7 +208,7 @@
 
 ;;;_  . A-?
 
-(define-key key-translation-map (kbd "A-TAB") (kbd "M-TAB"))
+(define-key key-translation-map (kbd "A-TAB") (kbd "C-TAB"))
 
 ;;;_ , ctl-x-map
 
@@ -1084,11 +1084,51 @@
 (use-package css-mode
   :mode ("\\.css$" . css-mode))
 
-;;;_ , delsel
+;;;_ , iflipb
 
-(use-package delsel
+(use-package iflipb
+  :commands (iflipb-next-buffer iflipb-previous-buffer)
+  :bind (("A-<tab>" . my-iflipb-next-buffer)
+         ("A-S-<tab>" . my-iflipb-previous-buffer))
   :init
-  (delete-selection-mode 1))
+  (progn
+    (defvar my-iflipb-auto-off-timeout-sec 1)
+    (defvar my-iflipb-auto-off-timer-canceler-internal nil)
+    (defvar my-iflipb-ing-internal nil)
+
+    (defun my-iflipb-auto-off ()
+      (message nil)
+      (setq my-iflipb-auto-off-timer-canceler-internal nil
+            my-iflipb-ing-internal nil))
+
+    (defun my-iflipb-next-buffer (arg)
+      (interactive "P")
+      (iflipb-next-buffer arg)
+      (if my-iflipb-auto-off-timer-canceler-internal
+          (cancel-timer my-iflipb-auto-off-timer-canceler-internal))
+      (run-with-idle-timer my-iflipb-auto-off-timeout-sec 0 'my-iflipb-auto-off)
+      (setq my-iflipb-ing-internal t))
+
+    (defun my-iflipb-previous-buffer ()
+      (interactive)
+      (iflipb-previous-buffer)
+      (if my-iflipb-auto-off-timer-canceler-internal
+          (cancel-timer my-iflipb-auto-off-timer-canceler-internal))
+      (run-with-idle-timer my-iflipb-auto-off-timeout-sec 0 'my-iflipb-auto-off)
+      (setq my-iflipb-ing-internal t)))
+
+  :config
+  (progn
+    (setq iflipb-always-ignore-buffers
+          "\\`\\( \\|diary\\|ipa\\|\\.newsrc-dribble\\'\\)"
+          iflipb-wrap-around t)
+
+    (defun iflipb-first-iflipb-buffer-switch-command ()
+      "Determines whether this is the first invocation of
+iflipb-next-buffer or iflipb-previous-buffer this round."
+      (not (and (or (eq last-command 'my-iflipb-next-buffer)
+                    (eq last-command 'my-iflipb-previous-buffer))
+                my-iflipb-ing-internal)))))
 
 ;;;_ , diff-mode
 
