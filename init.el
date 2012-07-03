1deacc4fc249baaf2ac5f6a50d6b14dbe93f45f1
John Wiegley <johnw@newartisans.com>
2012-07-02 23:02:49 -0500

    Created `lusty-read-directory', with some hacking

--- init.el
+++ init.el
@@ -2388,6 +2388,51 @@ FORM => (eval FORM)."
              (lusty-select-current-name)))
           (:buffer-explorer (lusty-select-match)))))
 
+    (defvar lusty-only-directories nil)
+
+    (defun lusty-file-explorer-matches (path)
+      (let* ((dir (lusty-normalize-dir (file-name-directory path)))
+             (file-portion (file-name-nondirectory path))
+             (files
+              (and dir
+                   ;; NOTE: directory-files is quicker but
+                   ;;       doesn't append slash for directories.
+                   ;;(directory-files dir nil nil t)
+                   (file-name-all-completions "" dir)))
+             (filtered (lusty-filter-files
+                        file-portion
+                        (if lusty-only-directories
+                            (loop for f in files
+                                  when (= ?/ (aref f (1- (length f))))
+                                  collect f)
+                          files))))
+        (if (or (string= file-portion "")
+                (string= file-portion "."))
+            (sort filtered 'string<)
+          (lusty-sort-by-fuzzy-score filtered file-portion))))
+
+    (defun lusty-read-directory ()
+      "Launch the file/directory mode of LustyExplorer."
+      (interactive)
+      (let ((lusty--active-mode :file-explorer))
+        (lusty--define-mode-map)
+        (let* ((lusty--ignored-extensions-regex
+                (concat "\\(?:" (regexp-opt completion-ignored-extensions) "\\)$"))
+               (minibuffer-local-filename-completion-map lusty-mode-map)
+               (lusty-only-directories t))
+          (lusty--run 'read-directory-name default-directory ""))))
+
+    (defun lusty-read-file-name ()
+      "Launch the file/directory mode of LustyExplorer."
+      (interactive)
+      (let ((lusty--active-mode :file-explorer))
+        (lusty--define-mode-map)
+        (let* ((lusty--ignored-extensions-regex
+                (concat "\\(?:" (regexp-opt completion-ignored-extensions) "\\)$"))
+               (minibuffer-local-filename-completion-map lusty-mode-map)
+               (lusty-only-directories nil))
+          (lusty--run 'read-file-name default-directory ""))))
+
     (if (featurep 'icicles)
         (defadvice lusty-file-explorer (around lusty-file-explorer-without-icy
                                                activate)
