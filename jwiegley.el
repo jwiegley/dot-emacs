3ef2b4fd82821af29e42eb51348b4d9ac112ee23
Tony Day <zygomega@gmail.com>
2012-09-16 17:36:42 +1000

    Added personal config file to get working out of box.
    Borrowed some patterns from starter-kit to get dotemacs to work out of
    the box:
    init.el : added a test to find out start-dir.  This means that the
    dotemcs will work either as init files or as a load.
    load-path.el : will work without override and misc directories
    jwiegley.el : for username jwiegley works as before ;)

---
+++ jwiegley.el
@@ -0,0 +1,23 @@
+
+(setq override-dirs
+      (list
+       "override/bbdb/lisp/"
+       "override/gnus/contrib/"
+       "override/gnus/lisp/"
+       "override/org-mode/contrib/lisp/"
+       "override/org-mode/lisp/"
+       "override/tramp/lisp/"))
+
+;; Packages located elsewhere on the system...
+
+;;(setq misc-dirs nil)
+
+(setq misc-dirs (list
+                 "~/src/ledger/lisp/"
+                 "/usr/local/share/emacs/site-lisp/"
+                 "/opt/local/share/emacs/site-lisp/"
+                 "/opt/local/share/doc/git-core/contrib/emacs/"
+                 "/Users/johnw/Archives/Languages/Ruby/Sources/ruby/misc/"
+                 "/opt/local/share/texmf-texlive-dist/doc/latex/latex2e-help-texinfo/"
+                 ))
+ 
