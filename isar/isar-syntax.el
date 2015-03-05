33b7700a53e9e3dc11538f52073cbcf05a618960
David Aspinall <da@inf.ed.ac.uk>
2015-03-05 09:24:01 +0000

    Fix colours for dark backgrounds

--- isar/isar-syntax.el
+++ isar/isar-syntax.el
@@ -290,7 +290,7 @@ matches contents of quotes for quoted identifiers.")
 (defface isabelle-string-face 
   (proof-face-specs
    (:foreground "springgreen4")
-   (:background "springgreen1")
+   (:foreground "springgreen1")
    ())
   "*Face for fontifying string contents in Isabelle."
   :group 'proof-faces)
@@ -298,7 +298,7 @@ matches contents of quotes for quoted identifiers.")
 (defface isabelle-quote-face 
   (proof-face-specs
    (:foreground "Gray80")
-   (:background "Gray30")
+   (:foreground "Gray30")
    (:italic t))
   "*Face for quotes (string delimiters) in Isabelle."
   :group 'proof-faces)
@@ -330,7 +330,7 @@ matches contents of quotes for quoted identifiers.")
 (defface isabelle-free-name-face
   (proof-face-specs
    (:foreground "blue")
-   (:foreground "blue3")
+   (:foreground "lightblue")
    (:bold t))
   "*Face for Isabelle term / type highlighting"
   :group 'proof-faces)
