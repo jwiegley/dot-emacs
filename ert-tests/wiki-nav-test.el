d342dde751423613e8c6ef0503b5b638aa627e1f
John Wiegley <johnw@newartisans.com>
2015-03-13 02:39:04 -0500

    Squashed 'lib/button-lock/' content from commit f9082fe
    git-subtree-dir: lib/button-lock
    git-subtree-split: f9082feb329432fcf2ac49a95e64bed9fda24d58

---
+++ ert-tests/wiki-nav-test.el
@@ -0,0 +1,55 @@
+
+;;; requires and setup
+
+(require 'button-lock)
+(require 'wiki-nav)
+
+;;; this is a stub - no tests defined
+
+;;; wiki-nav-point-before ()
+
+;;; wiki-nav-comment-only-mode-p ()
+
+;;; wiki-nav-alist-flatten (list)
+
+;;; wiki-nav-buffer-included-p (buf)
+
+;;; wiki-nav-link-set (&optional arg)
+
+;;; wiki-nav-links (&optional buffer)
+
+;;; wiki-nav-links-all-buffers ()
+
+;;; wiki-nav-default-multi-action (event)
+
+;;; wiki-nav-action-1 (pos)
+
+;;; wiki-nav-mouse-action (event)
+
+;;; wiki-nav-keyboard-action ()
+
+;;; wiki-nav-maybe-turn-on (&optional arg)
+
+;;; wiki-nav-find-any-link (&optional arg)
+
+;;; wiki-nav-find-any-previous-link ()
+
+;;; wiki-nav-ido (arg)
+
+;;; wiki-nav-mode
+
+;;; global-wiki-nav-mode
+
+;;
+;; Emacs
+;;
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; mangle-whitespace: t
+;; require-final-newline: t
+;; coding: utf-8
+;; byte-compile-warnings: (not cl-functions)
+;; End:
+;;
+
+;;; wiki-nav-test.el ends here
