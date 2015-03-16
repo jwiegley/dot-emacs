93689af4dfe7cf3a31a8514bf1b8ff251d527b5e
John Wiegley <jwiegley@gmail.com>
2015-03-16 14:02:00 -0500

    Use #' in one place

--- init.el
+++ init.el
@@ -1378,12 +1378,12 @@
   (eval-when-compile
     (defvar exit-status))
   (add-hook 'compilation-finish-functions
-            (lambda (buf why)
-              (display-buffer buf)
-              (if (> exit-status 0)
-                  (alert "Compilation finished with errors"
-                         :buffer buf :severity 'high)
-                (alert "Compilation finished" :buffer buf)))))
+            #'(lambda (buf why)
+                (display-buffer buf)
+                (if (> exit-status 0)
+                    (alert "Compilation finished with errors"
+                           :buffer buf :severity 'high)
+                  (alert "Compilation finished" :buffer buf)))))
 
 ;;;_ , color-moccur
 
