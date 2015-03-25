fb2d142f39cfb9add722843528481232f87905f0
John Wiegley <johnw@newartisans.com>
2015-03-24 21:59:40 -0500

    Squashed 'site-lisp/web-mode/' content from commit 2918840
    git-subtree-dir: site-lisp/web-mode
    git-subtree-split: 2918840a16b890ad6d069e2add51f660553e2d29

---
+++ tests/test.js
@@ -0,0 +1,95 @@
+var cs = {completed: this.props.todo.completed,
+          editing: this.props.editing};
+
+return {
+  nowShowing: Const.ALL_TODOS,
+  editing: null
+};
+
+
+if (true) {
+  var x;
+  /*
+   * Hello there.
+   * Today is Saturday.
+   */
+  var z;
+  /* lorem
+     ipsum */
+  var y;
+}
+
+var x = y,
+    cd = 3;
+if (true) {
+  var x = y,
+      cd = 3;
+  export const {x, y} = z,
+      a = 10;
+}
+
+var pathData = this.barData
+                   .map(function(d) {
+                     return [{x: d.start_time_ms, vol: d.volume},
+                             {x: d.end_time_ms, vol: d.volume}];
+                   })
+                   .reduce(function(a, b) {
+                     return a.concat(b);
+                   });
+
+function toto(c) {
+  switch(elem) {
+    case "textarea" :
+      return ( <textarea rows="4" /> );
+      break;
+    case "text" :
+      return ( <input type="text" /> );
+    default :
+      return null;
+  }
+}
+
+function hello() {
+  if (x)
+    console.log("lorem");
+  else
+    console.log("ipsum");
+}
+
+var obj = {
+  "a": 1,
+  "b": 2,
+  "c": 3
+};
+
+var arr = [1,
+           2,
+           3
+];
+
+function onYouTubePlayerAPIReady(videoid) {
+  ytPlayer = new YT.Player('media_area',
+                           {videoId: videoid,
+                            playerVars: { 'rel': 0 }
+                           });
+}
+
+var newB = new example();
+
+newB.firstMethod('x')
+    .secondMethod('y')
+    .thirdMethod(1,15,'z');
+
+var bar = 1;
+var foo = (bar == 1) ?
+          "ONE" : "OTHER";  // THIS IS THE PROBLEM
+
+function fieldIsValid( field, fieldSpec) {
+  // todo: add remote possibility
+  var value = field.value;
+  if ( value.length < fieldSpec.minNumberOfChar ||
+       value.length > fieldSpec.maxNumberOfChar ||
+       value.match( fieldSpec.pattern) )
+    return true;
+  return false;
+}
