6ce76c2b2eac32f0fdc44b0bf7c3ff6a2163b235
John Wiegley <johnw@newartisans.com>
2015-03-31 14:39:24 -0500

    Remove more files

--- site-lisp/hsenv/src/Util/List.hs
+++
@@ -1,7 +0,0 @@
-module Util.List (breakOn) where
-
-breakOn :: Eq a => a -> [a] -> Maybe ([a], [a])
-breakOn sep = aux []
-  where aux _ [] = Nothing
-        aux prevs (x:xs) | x == sep   = Just (reverse prevs, xs)
-                         | otherwise = aux (x:prevs) xs
--- site-lisp/hsenv/src/Util/String.hs
+++
@@ -1,5 +0,0 @@
-module Util.String (padTo) where
-
-padTo :: String -> Int -> String
-padTo s n | length s < n = take n $ s ++ repeat ' '
-          | otherwise    = s
