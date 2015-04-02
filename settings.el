93f0c50783479928fe77e800220134d94b80a718
John Wiegley <jwiegley@gmail.com>
2015-04-01 23:48:34 -0500

    Lower ggtags-oversize-limit

--- settings.el
+++ settings.el
@@ -551,6 +551,7 @@
  '(gc-cons-threshold 3500000)
  '(gdb-find-source-frame t)
  '(gdb-same-frame nil)
+ '(ggtags-oversize-limit 1048576)
  '(ggtags-use-sqlite3 t)
  '(git-commit-mode-hook
    (quote
