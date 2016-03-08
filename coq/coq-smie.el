0443734841b337b3695de02275b1a2500931358b
Pierre Courtieu <Pierre.Courtieu@cnam.fr>
2016-03-08 15:39:42 +0100

    Avoiding useless computation in indentation code.

--- coq/coq-smie.el
+++ coq/coq-smie.el
@@ -464,7 +464,8 @@ The point should be at the beginning of the command name."
 (defun coq-smie-backward-token ()
   (let* ((tok (smie-default-backward-token))
 	 (start (save-excursion (coq-find-real-start) (point)))
-	 (is-tactic (coq-smie-is-tactic)))
+	 ;(is-tactic (coq-smie-is-tactic)) ; useless and morevoer expensive if at each call
+         )
     (cond
      ;; Distinguish between "," from quantification and other uses of
      ;; "," (tuples, tactic arguments)
