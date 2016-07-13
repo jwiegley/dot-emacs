718a819cdd8ff70b48f684d6c31cf55fd1f3c2d0
John Wiegley <johnw@newartisans.com>
2016-07-13 09:56:31 -0700

    Use dockerfile-mode

--- init.el
+++ init.el
@@ -1514,6 +1514,10 @@ Inspired by Erik Naggum's `recursive-edit-with-single-window'."
   :config
   (add-hook 'dired-toggle-mode-hook #'my-dired-toggle-mode-hook))
 
+(use-package dockerfile-mode
+  :mode ("\\`Dockerfile\\'" . dockerfile-mode)
+  :load-path "site-lisp/dockerfile-mode/")
+
 (use-package doxymacs
   :disabled t
   :load-path "site-lisp/doxymacs/lisp/")
@@ -3792,7 +3796,29 @@ Inspired by Erik Naggum's `recursive-edit-with-single-window'."
   :load-path "override/tramp"
   :defer t
   :config
-  (add-to-list 'tramp-remote-path "/run/current-system/sw/bin"))
+  (add-to-list 'tramp-remote-path "/run/current-system/sw/bin")
+
+  ;; Open files in Docker containers like so: /docker:drunk_bardeen:/etc/passwd
+  (push
+   (cons
+    "docker"
+    '((tramp-login-program "docker")
+      (tramp-login-args (("exec" "-it") ("%h") ("/bin/bash")))
+      (tramp-remote-shell "/bin/sh")
+      (tramp-remote-shell-args ("-i") ("-c"))))
+   tramp-methods)
+
+  (defadvice tramp-completion-handle-file-name-all-completions
+      (around dotemacs-completion-docker activate)
+    "(tramp-completion-handle-file-name-all-completions \"\" \"/docker:\" returns
+    a list of active Docker container names, followed by colons."
+    (if (equal (ad-get-arg 1) "/docker:")
+        (let* ((dockernames-raw (shell-command-to-string "docker ps | perl -we 'use strict; $_ = <>; m/^(.*)NAMES/ or die; my $offset = length($1); while(<>) {substr($_, 0, $offset, q()); chomp; for(split m/\\W+/) {print qq($_:\n)} }'"))
+               (dockernames (cl-remove-if-not
+                             #'(lambda (dockerline) (string-match ":$" dockerline))
+                             (split-string dockernames-raw "\n"))))
+          (setq ad-return-value dockernames))
+      ad-do-it)))
 
 (use-package vkill
   :commands vkill
