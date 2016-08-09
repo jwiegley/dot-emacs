4c409ceeeaaf7c6ddd61fb3f542d4fb8248c9199
John Wiegley <johnw@newartisans.com>
2016-08-09 08:36:32 -0700

    Set (ns-alternate-modifier (quote none))

--- settings.el
+++ settings.el
@@ -796,7 +796,7 @@
    (quote
     (gnus-thread-sort-by-most-recent-date gnus-thread-sort-by-total-score)))
  '(next-line-add-newlines nil)
- '(ns-alternate-modifier (quote alt))
+ '(ns-alternate-modifier (quote none))
  '(ns-command-modifier (quote meta))
  '(ns-right-alternate-modifier (quote super))
  '(ns-right-control-modifier (quote hyper))
