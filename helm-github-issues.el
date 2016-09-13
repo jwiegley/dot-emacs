1cfea3f5dcac6ea2bfff52464e8dc295ff5a3d17
John Wiegley <johnw@newartisans.com>
2016-09-13 10:25:00 -0700

    Squashed 'site-lisp/helm-github-issues/' changes from 347f846..17b1e61
    17b1e61 Add Cask file and ignore list
    effb24f Update for latest helm
    
    git-subtree-dir: site-lisp/helm-github-issues
    git-subtree-split: 17b1e61e2f25a9754ab2552e9ea09f4d2025f265

--- helm-github-issues.el
+++ helm-github-issues.el
@@ -1,11 +1,11 @@
-;;; helm-github-issues.el --- github issues viewer with helm interface
+;;; helm-github-issues.el --- github issues viewer with helm interface -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2013 by Syohei YOSHIDA
+;; Copyright (C) 2015 by Syohei YOSHIDA
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-helm-github-issues
 ;; Version: 0.01
-;; Package-Requires: ((helm "1.0") (gh "1.0"))
+;; Package-Requires: ((helm-core "1.7.7") (gh "1.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -24,7 +24,7 @@
 
 ;;; Code:
 
-(require 'cl)
+(require 'cl-lib)
 
 (require 'helm)
 (require 'gh-issues)
@@ -38,101 +38,102 @@
   "Github API"
   :group 'helm-github-issues)
 
-(defvar helm-c-github-issues-repo-list nil)
+(defvar helm-github-issues-repo-list nil)
+(defvar helm-github-issues-repo)
 
-(defun helm-c-github-issues-default-repo ()
+(defun helm-github-issues-default-repo ()
   (with-temp-buffer
     (when (zerop (call-process-shell-command "git remote -v" nil t))
       (goto-char (point-min))
       (when (re-search-forward "github\\.com[/:]?\\([^/]+/.+?\\)\\.git" nil t)
         (match-string-no-properties 1)))))
 
-(defun helm-c-github-issues-filter-issues (issues state)
+(defun helm-github-issues-filter-issues (issues state)
   (if (string= state "all")
       issues
-    (remove-if-not (lambda (issue)
-                     (string= (oref issue state) state)) issues)))
+    (cl-remove-if-not (lambda (issue)
+                        (string= (oref issue state) state)) issues)))
 
 (defcustom helm-github-issues-bookmarks nil
   "Bookmarks of projects"
   :type 'list
   :group 'helm-github-issues)
 
-(defun helm-c-github-issues-init ()
-  (let ((user-repo-str (completing-read "Repositry(user/repo): "
-                                        helm-github-issues-bookmarks
-                                        nil nil nil
-                                        helm-c-github-issues-repo-list
-                                        (helm-c-github-issues-default-repo)))
-        (state (if current-prefix-arg
+(defun helm-github-issues-init ()
+  (let ((state (if current-prefix-arg
                    (completing-read "Issue type: "
                                     '("all" "open" "close")
                                     nil t nil nil "all")
                  "all")))
-    (let ((user-repo (split-string user-repo-str "/")))
-      (assert (= (length user-repo) 2))
-      (let* ((user (first user-repo))
-             (repo (second user-repo))
+    (let ((user-repo (split-string helm-github-issues-repo "/")))
+      (cl-assert (= (length user-repo) 2))
+      (let* ((user (cl-first user-repo))
+             (repo (cl-second user-repo))
              (issues (gh-issues-issue-list helm-github-issues-api user repo)))
         (if (null issues)
             (error "This repo has no issues!!")
-          (sort (helm-c-github-issues-filter-issues (oref issues data) state)
+          (sort (helm-github-issues-filter-issues (oref issues data) state)
                 (lambda (a b)
                   (< (oref a number) (oref b number)))))))))
 
-(defun helm-c-github-issues-real-to-display (issue)
+(defun helm-github-issues-real-to-display (issue)
   (with-slots (number title state) issue
     (format "#%-4d [%s] %s" number state title)))
 
-(defun helm-c-github-issues-chomp (str)
+(defun helm-github-issues-chomp (str)
   (replace-regexp-in-string "\r$" "" str))
 
-(defvar helm-c-github-issues-headers
+(defvar helm-github-issues-headers
   (mapcar (lambda (str)
             (format "%-6s :" (propertize str 'face 'highlight)))
           '("Title" "Number" "User" "Status")))
 
-(defun helm-c-github-issues-format (title number user state)
+(defun helm-github-issues-format (title number user state)
   (mapconcat 'identity
-             (mapcar* (lambda (header value)
-                        (format "%s %s" header value))
-                      helm-c-github-issues-headers
-                      (list title number user state))
+             (cl-mapcar (lambda (header value)
+                          (format "%s %s" header value))
+                        helm-github-issues-headers
+                        (list title number user state))
              "\n"))
 
-(defun helm-c-github-issues-persistent-action (issue)
+(defun helm-github-issues-persistent-action (issue)
   (with-current-buffer (get-buffer-create "*Help Github Issues*")
     (erase-buffer)
     (goto-char (point-min))
     (with-slots (title number user state body) issue
-      (insert (helm-c-github-issues-format
+      (insert (helm-github-issues-format
                title number (oref user login) state))
       (insert "\n\n")
       (let ((before-body (point)))
-        (insert (helm-c-github-issues-chomp body))
+        (insert (helm-github-issues-chomp body))
         (fill-region before-body (point-max)))))
   (with-help-window (help-buffer)
     (princ (with-current-buffer "*Help Github Issues*"
              (buffer-string)))))
 
-(defun helm-c-github-issues-construct-url (url)
+(defun helm-github-issues-construct-url (url)
   (replace-regexp-in-string
    "api\\." ""
    (replace-regexp-in-string "/repos" "" url)))
 
-(defvar helm-c-github-issues-source
-  '((name . "Github Issues")
-    (candidates . helm-c-github-issues-init)
-    (volatile)
-    (real-to-display . helm-c-github-issues-real-to-display)
-    (persistent-action . helm-c-github-issues-persistent-action)
-    (action . (lambda (issue)
-                (browse-url (helm-c-github-issues-construct-url
-                             (oref issue url)))))))
+(defvar helm-github-issues-source
+  (helm-build-sync-source "Github Issues"
+    :candidates #'helm-github-issues-init
+    :real-to-display #'helm-github-issues-real-to-display
+    :persistent-action #'helm-github-issues-persistent-action
+    :action (lambda (issue)
+              (browse-url (helm-github-issues-construct-url
+                           (oref issue url))))))
 
+;;;###autoload
 (defun helm-github-issues ()
   (interactive)
-  (helm :sources '(helm-c-github-issues-source) :buffer "*helm github issues*"))
+  (let ((helm-github-issues-repo (completing-read "Repositry(user/repo): "
+                                                  helm-github-issues-bookmarks
+                                                  nil nil nil
+                                                  helm-github-issues-repo-list
+                                                  (helm-github-issues-default-repo))))
+    (helm :sources '(helm-github-issues-source) :buffer "*helm github issues*")))
 
 (provide 'helm-github-issues)
 
