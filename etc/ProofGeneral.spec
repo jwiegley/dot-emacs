5d7c77ae029de273724248105f416d57983631b2
Erik Martin-Dorel <erik@martin-dorel.org>
2016-09-19 18:27:16 +0200

    Bump version number for next release cycle.

--- etc/ProofGeneral.spec
+++ etc/ProofGeneral.spec
@@ -1,6 +1,6 @@
 Summary:	Proof General, Emacs interface for Proof Assistants
 Name:		ProofGeneral
-Version:	4.4
+Version:	4.4.1~pre
 Release:	1
 Group:		Text Editors/Integrated Development Environments (IDE)
 License:	GPL
--- generic/proof-site.el
+++ generic/proof-site.el
@@ -72,7 +72,7 @@
 
 (eval-and-compile
   ;; WARNING: do not edit next line (constant is edited in Makefile.devel)
-  (defconst proof-general-version "Proof General Version 4.4pre."
+  (defconst proof-general-version "Proof General Version 4.4.1~pre."
     "Version string identifying Proof General release."))
 
 (defconst proof-general-short-version
