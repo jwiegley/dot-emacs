c1f6c9c2f87edea03d9c279719f0f4a10c396db0
Clément Pit--Claudel <clement.pitclaudel@live.com>
2017-02-25 19:55:51 -0500

    Remove uses of defpacustom in coq-compile-common
    This file is `require'-d from other places, at compile time.  This doesn't work
    nicely with compilation by package.el (PG complains and says "defpacustom: Proof
    assistant setting compile-before-require re-defined!")

--- coq/coq-seq-compile.el
+++ coq/coq-seq-compile.el
@@ -19,10 +19,7 @@
   (require 'proof-compat))
 
 (eval-when (compile)
-  (defvar queueitems)       ; dynamic scope in p-s-extend-queue-hook
-  (defvar coq-compile-before-require)       ; defpacustom
-  (defvar coq-compile-parallel-in-background)       ; defpacustom
-  (defvar coq-confirm-external-compilation)); defpacustom
+  (defvar queueitems))       ; dynamic scope in p-s-extend-queue-hook
 
 (require 'coq-compile-common)
 
