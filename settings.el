40a47c6abdcaaa8734a2e56d577aa66bef554569
John Wiegley <johnw@newartisans.com>
2017-07-05 16:55:56 -0700

    changes

--- settings.el
+++ settings.el
@@ -603,6 +603,7 @@
  '(find-ls-option (quote ("-print0 | xargs -0 ls -ld" . "-ld")))
  '(find-ls-subdir-switches "-alh")
  '(flx-ido-use-faces nil)
+ '(flycheck-coq-executable "~/bin/ct-coqtop")
  '(flycheck-display-errors-delay 0.0)
  '(flycheck-standard-error-navigation nil)
  '(flymake-compilation-prevents-syntax-check nil)
