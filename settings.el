7874591febe3e3a67f0dedb781ff6eaccbb542fb
John Wiegley <johnw@newartisans.com>
2017-08-01 09:32:57 -0700

    changes

--- settings.el
+++ settings.el
@@ -300,7 +300,9 @@
     ("^ \\*Minibuf-.*\\*$" "^\\*Summary" "^\\*Article" "^#")))
  '(clean-buffer-list-kill-regexps (quote (".*")))
  '(column-number-mode t)
- '(company-coq-disabled-features (quote (hello prettify-symbols dynamic-symbols-backend)))
+ '(company-coq-disabled-features
+   (quote
+    (hello prettify-symbols smart-subscripts dynamic-symbols-backend)))
  '(company-coq-prettify-symbols-alist
    (quote
     (("|-" . 8866)
@@ -351,6 +353,7 @@
    (quote
     (".glob" ".vo" ".v.d" ".o" "~" ".bin" ".lbin" ".so" ".a" ".ln" ".blg" ".bbl" ".elc" ".lof" ".glo" ".idx" ".lot" ".svn/" ".hg/" ".git/" ".bzr/" "CVS/" "_darcs/" "_MTN/" ".fmt" ".tfm" ".class" ".fas" ".lib" ".mem" ".x86f" ".sparcf" ".dfsl" ".pfsl" ".d64fsl" ".p64fsl" ".lx64fsl" ".lx32fsl" ".dx64fsl" ".dx32fsl" ".fx64fsl" ".fx32fsl" ".sx64fsl" ".sx32fsl" ".wx64fsl" ".wx32fsl" ".fasl" ".ufsl" ".fsl" ".dxl" ".lo" ".la" ".gmo" ".mo" ".toc" ".aux" ".cp" ".fn" ".ky" ".pg" ".tp" ".vr" ".cps" ".fns" ".kys" ".pgs" ".tps" ".vrs" ".pyc" ".pyo")))
  '(coq-compile-before-require t)
+ '(coq-compile-parallel-in-background t)
  '(coq-holes-minor-mode nil)
  '(coq-maths-menu-enable t)
  '(coq-one-command-per-line nil)
@@ -362,8 +365,8 @@
  '(custom-raised-buttons nil)
  '(default-frame-alist
     (quote
-     ((font . "-*-Source Code Pro-normal-normal-normal-*-15-*-*-*-m-0-iso10646-1")
-      (cursor-color . "#b247ee"))))
+     ((cursor-color . "#b247ee")
+      (font . "-*-Hack-normal-normal-normal-*-16-*-*-*-m-0-iso10646-1"))))
  '(default-input-method "latin-1-prefix")
  '(default-major-mode (quote text-mode) t)
  '(delete-by-moving-to-trash t)
@@ -1181,6 +1184,8 @@
  '(diff-header ((((background dark)) (:foreground "Cyan")) (t (:foreground "Red"))))
  '(diff-index ((((background dark)) (:foreground "Magenta")) (t (:foreground "Green"))))
  '(diff-nonexistent ((((background dark)) (:foreground "#FFFFFFFF7474")) (t (:foreground "DarkBlue"))))
+ '(font-lock-comment-face ((t (:foreground "grey50" :slant italic))))
+ '(font-lock-doc-face ((t (:foreground "purple4"))))
  '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 2.0))))
  '(markdown-header-face-2 ((t (:inherit markdown-header-face :height 1.6))))
  '(markdown-header-face-3 ((t (:inherit markdown-header-face :height 1.4))))
