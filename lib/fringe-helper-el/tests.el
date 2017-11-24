da6cf553803057261ecca5ad04f5082575b5bea3
John Wiegley <johnw@newartisans.com>
2017-11-24 00:29:24 -0800

    Removed lib/fringe-helper-el to subtree add at this location

--- lib/fringe-helper-el/tests.el
+++
@@ -1,7 +0,0 @@
-(require 'ert)
-(require 'fringe-helper)
-
-(ert-deftest fringe-helper-convert-test ()
-  (should (equal [0 15 240 255]
-                 (fringe-helper-convert "........" "....XXXX"
-                                        "XXXX...." "XXXXXXXX"))))
