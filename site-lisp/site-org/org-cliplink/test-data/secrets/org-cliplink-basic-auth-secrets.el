cf084bf0421a13aabfdd7bf3ee4ef9af4f6069f5
John Wiegley <johnw@newartisans.com>
2017-11-24 00:03:14 -0800

    Remove files, going back to top-level site-lisp

--- site-lisp/site-org/org-cliplink/test-data/secrets/org-cliplink-basic-auth-secrets.el
+++
@@ -1,6 +0,0 @@
-(:basic-auth ((:url-pattern "https://127.0.0.1:4663/hello/*"
-               :username "foo"
-               :password "bar")
-              (:url-pattern "http://rexim.me/*"
-               :username "horta"
-               :password "hell")))
