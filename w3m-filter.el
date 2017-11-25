3225ed6f7f7ec842fd973761372d653366f93f9b
John Wiegley <johnw@newartisans.com>
2017-11-25 13:46:22 -0800

    Squashed 'site-lisp/emacs-w3m/' changes from 299bc0d57b..83f5237263
    83f5237263 w3m-filter-add-name-anchors: Improve regexp [emacs-w3m:12812]
    
    git-subtree-dir: site-lisp/emacs-w3m
    git-subtree-split: 83f5237263400dc03ecff2c40962a79e486bb8f2

--- w3m-filter.el
+++ w3m-filter.el
@@ -448,7 +448,8 @@ href=\"\\)\\(?:[^\"]+\\)?/\\(?:imgres\\?imgurl\\|url\\?\\(?:q\\|url\\)\\)=\
 	  (goto-char (point-max)))))))
 
 (defun w3m-filter-add-name-anchors (url)
-  ;;  cf. [emacs-w3m:11153] [emacs-w3m:12339] [emacs-w3m:12422]
+  ;;  cf. [emacs-w3m:11153], [emacs-w3m:12339], [emacs-w3m:12422],
+  ;; [emacs-w3m:12812]
   "Add name anchors that w3m can handle.
 This function adds ``<a name=\"FOO_BAR\"></a>'' in front of
 ``<TAG ... id=\"FOO_BAR\" ...>FOO BAR</TAG>'' in the current buffer."
@@ -457,7 +458,7 @@ This function adds ``<a name=\"FOO_BAR\"></a>'' in front of
 	names regexp i st nd)
     (goto-char (point-min))
     (while (re-search-forward "<a[\t\n\r ]+\\(?:[^\t\n\r >]+[\t\n\r ]+\\)*\
-href=\"#\\([a-z][-.0-9:_a-z]*\\)\"" nil t)
+href=\"#\\([.a-z][-.0-9:_a-z]*\\)\"" nil t)
       (add-to-list 'names (match-string 1)))
     (setq case-fold-search nil)
     (while names
