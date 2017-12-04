efbc3c6edf76362484cfa1df1738157a03bde978
John Wiegley <johnw@newartisans.com>
2017-12-04 00:53:34 -0800

    Changes

--- init.el
+++ init.el
@@ -3161,26 +3161,26 @@ non-empty directories is allowed."
   :load-path "site-lisp/yari-with-buttons"
   :commands yari)
 
-;; (use-package yasnippet
-;;   :load-path "site-lisp/yasnippet"
-;;   :demand t
-;;   :diminish yas-minor-mode
-;;   :bind (("C-c y d" . yas-load-directory)
-;;          ("C-c y i" . yas-insert-snippet)
-;;          ("C-c y f" . yas-visit-snippet-file)
-;;          ("C-c y n" . yas-new-snippet)
-;;          ("C-c y t" . yas-tryout-snippet)
-;;          ("C-c y l" . yas-describe-tables)
-;;          ("C-c y g" . yas/global-mode)
-;;          ("C-c y m" . yas/minor-mode)
-;;          ("C-c y a" . yas-reload-all)
-;;          ("C-c y x" . yas-expand))
-;;   :bind (:map yas-keymap
-;;               ("C-i" . yas-next-field-or-maybe-expand))
-;;   :mode ("/\\.emacs\\.d/snippets/" . snippet-mode)
-;;   :config
-;;   (yas-load-directory "~/.emacs.d/snippets/")
-;;   (yas-global-mode 1))
+(use-package yasnippet
+  :load-path "site-lisp/yasnippet"
+  :demand t
+  :diminish yas-minor-mode
+  :bind (("C-c y d" . yas-load-directory)
+         ("C-c y i" . yas-insert-snippet)
+         ("C-c y f" . yas-visit-snippet-file)
+         ("C-c y n" . yas-new-snippet)
+         ("C-c y t" . yas-tryout-snippet)
+         ("C-c y l" . yas-describe-tables)
+         ("C-c y g" . yas/global-mode)
+         ("C-c y m" . yas/minor-mode)
+         ("C-c y a" . yas-reload-all)
+         ("C-c y x" . yas-expand))
+  :bind (:map yas-keymap
+              ("C-i" . yas-next-field-or-maybe-expand))
+  :mode ("/\\.emacs\\.d/snippets/" . snippet-mode)
+  :config
+  (yas-load-directory "~/.emacs.d/snippets/")
+  (yas-global-mode 1))
 
 (use-package yasnippet-snippets
   :load-path "site-lisp/yasnippet-snippets"
