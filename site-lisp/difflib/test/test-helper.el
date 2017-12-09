4e0abcc978eceb3ca4640d798f43719602074100
John Wiegley <johnw@newartisans.com>
2017-12-09 13:50:43 -0800

    Move difflib

--- site-lisp/difflib/test/test-helper.el
+++
@@ -1,6 +0,0 @@
-;;; test-helper.el --- Helpers for difflib.el-test.el
-
-(let ((difflib-dir (f-parent (f-dirname (f-this-file)))))
-  (add-to-list 'load-path difflib-dir))
-
-;;; test-helper.el ends here
