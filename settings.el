8db581408ec0bf467a184f8a66e7c8b1793e429a
John Wiegley <johnw@newartisans.com>
2017-12-13 22:30:39 -0800

    Configure math-additional-units

--- settings.el
+++ settings.el
@@ -842,6 +842,16 @@
  '(markdown-command-needs-filename t)
  '(markdown-enable-math t)
  '(markdown-open-command "open-markdown")
+ '(math-additional-units
+   (quote
+    ((GiB "1024 * MiB" "Giga Byte")
+     (MiB "1024 * KiB" "Mega Byte")
+     (KiB "1024 * B" "Kilo Byte")
+     (B nil "Byte")
+     (Gib "1024 * Mib" "Giga Bit")
+     (Mib "1024 * Kib" "Mega Bit")
+     (Kib "1024 * b" "Kilo Bit")
+     (b "B / 8" "Bit"))) t)
  '(mc/list-file "~/.emacs.d/data/mc-lists.el")
  '(mediawiki-site-alist
    (quote
