3821c4055536a961b5d08794d075212b987a2adf
John Wiegley <johnw@newartisans.com>
2017-12-16 22:06:39 -0800

    Remove auth-password-store (it's part of Emacs 26)

--- site-lisp/auth-password-store/.dir-locals.el
+++
@@ -1,8 +0,0 @@
-((emacs-lisp-mode
-  (eval . (flycheck-mode))
-  (eval . (flycheck-cask-setup))
-  (eval . (checkdoc-minor-mode))
-  (indent-tabs-mode . nil)
-  (fill-column . 80)
-  (sentence-end-double-space . t)
-  (emacs-lisp-docstring-fill-column . 75)))
