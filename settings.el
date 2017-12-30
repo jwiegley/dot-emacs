58736eea84627af55bf52d2041ba538fb51831b5
John Wiegley <johnw@newartisans.com>
2017-12-30 13:03:15 -0800

    Remove multi-compile

--- settings.el
+++ settings.el
@@ -862,13 +862,6 @@
  '(modelinepos-column-limit 80)
  '(mudel-mode-hook (quote (mudel-add-scroll-to-bottom)))
  '(mudel-output-filter-functions (quote (ansi-color-process-output)))
- '(multi-compile-alist
-   (quote
-    (((string-match "concerto" default-directory)
-      ("build-TXRX" . "(cd ~/bae/concerto/solver && make clean && cabal build && ./dist/build/solver/solver --args test/TXRX.opts)"))
-     ((string-match "haskell-z3" default-directory)
-      ("build-Generate" . "(cd ~/src/haskell-z3 ; cabal build && ./dist/build/generate/generate --yaml generate/z3-api-4.5.0.yml)")))))
- '(multi-compile-history-file "/Users/johnw/.emacs.d/data/multi-compile.cache")
  '(multi-term-program "/usr/bin/screen")
  '(multi-term-program-switches "-DR")
  '(multi-term-scroll-show-maximum-output t)
