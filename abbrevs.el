78e82e574aa722efa47ccae3c85be698d793d646
John Wiegley <johnw@newartisans.com>
2018-01-01 12:28:53 -0800

    Clean up abbrevs file

---
+++ abbrevs.el
@@ -0,0 +1,24 @@
+;; -*- coding: utf-8 -*-
+
+(define-abbrev-table 'coq-mode-abbrev-table
+  '(("refl" "Proof. reflexivity. Qed.")))
+
+(define-abbrev-table 'message-mode-abbrev-table
+  '(("linux" "GNU/Linux")))
+
+(define-abbrev-table 'global-abbrev-table
+  '(("Abdul" "`Abdu'l-Bahá")
+    ("Allahu" "Alláh'u'Abhá")
+    ("Aqdas" "Kitáb-i-Aqdas")
+    ("Bahai" "Bahá'í")
+    ("Bahau" "Bahá'u'lláh")
+    ("Iqan" "Kitáb-i-Íqán")
+    ("LSA" "Local Spiritual Assembly")
+    ("NSA" "National Spiritual Assembly")
+    ("UHJ" "Universal House of Justice")))
+
+;; Local Variables:
+;; after-save-hook: (check-parens quietly-read-abbrev-file)
+;; End:
+
+;;; abbrevs.el ends here
