df17ef1cea261fe17c3da36ac36db681e19819f8
John Wiegley <johnw@newartisans.com>
2018-01-15 17:34:19 -0800

    Move some files

--- site-lisp/emacs-sos/CONTRIBUTING.md
+++
@@ -1,1 +0,0 @@
-Create an issue or submit a patch :)
--- site-lisp/emacs-sql-indent/test-data/pr39.sql
+++
@@ -1,8 +0,0 @@
-select pk_id
-       , case
-         when t1.c1 is null then  '(null)'
-         when t1.c1 in ('a', 'b','c','d','e') then 'a'
-         else t1.c1
-         end c1_alt
-  from t1
- limit 10;
