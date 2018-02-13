e2d6c0e48236efa1c292e4464af4b7de059bae50
John Wiegley <johnw@newartisans.com>
2018-02-13 14:17:55 -0800

    Override haskell-process-load-complete to work with GHC 8.2

--- init.el
+++ init.el
@@ -1788,7 +1788,56 @@
               '((haskell-types       . "\\(\\s-+\\)\\(::\\|∷\\)\\s-+")
                 (haskell-assignment  . "\\(\\s-+\\)=\\s-+")
                 (haskell-arrows      . "\\(\\s-+\\)\\(->\\|→\\)\\s-+")
-                (haskell-left-arrows . "\\(\\s-+\\)\\(<-\\|←\\)\\s-+"))))))
+                (haskell-left-arrows . "\\(\\s-+\\)\\(<-\\|←\\)\\s-+")))))
+
+  (defun haskell-process-load-complete
+      (session process buffer reload module-buffer &optional cont)
+    "Handle the complete loading response. BUFFER is the string of
+  text being sent over the process pipe. MODULE-BUFFER is the
+  actual Emacs buffer of the module being loaded."
+    (when (get-buffer (format "*%s:splices*" (haskell-session-name session)))
+      (with-current-buffer (haskell-interactive-mode-splices-buffer session)
+        (erase-buffer)))
+    (let* ((ok (cond
+                ((haskell-process-consume
+                  process
+                  "Ok, \\(?:\\([0-9]+\\|one\\)\\) modules? loaded\\.$")
+                 t)
+                ((haskell-process-consume
+                  process
+                  "Failed, \\(?:[0-9]+\\) modules? loaded\\.$")
+                 nil)
+                ((haskell-process-consume
+                  process
+                  "Ok, modules loaded: \\(.+\\)\\.$")
+                 t)
+                ((haskell-process-consume
+                  process
+                  "Failed, modules loaded: \\(.+\\)\\.$")
+                 nil)
+                (t
+                 (error (message "Unexpected response from haskell process.")))))
+           (modules (haskell-process-extract-modules buffer))
+           (cursor (haskell-process-response-cursor process))
+           (warning-count 0))
+      (haskell-process-set-response-cursor process 0)
+      (haskell-check-remove-overlays module-buffer)
+      (while
+          (haskell-process-errors-warnings module-buffer session process buffer)
+        (setq warning-count (1+ warning-count)))
+      (haskell-process-set-response-cursor process cursor)
+      (if (and (not reload)
+               haskell-process-reload-with-fbytecode)
+          (haskell-process-reload-with-fbytecode process module-buffer)
+        (haskell-process-import-modules process (car modules)))
+      (if ok
+          (haskell-mode-message-line (if reload "Reloaded OK." "OK."))
+        (haskell-interactive-mode-compile-error session "Compilation failed."))
+      (when cont
+        (condition-case-unless-debug e
+            (funcall cont ok)
+          (error (message "%S" e))
+          (quit nil))))))
 
 (use-package helm
   :defer t
