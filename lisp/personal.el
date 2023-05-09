be1a5f25cb33ef9454b4aded380a5cc1771fd556
John Wiegley <johnw@newartisans.com>
2023-05-08 21:58:46 -0700

    changes

--- lisp/personal.el
+++ lisp/personal.el
@@ -48,6 +48,23 @@
   (interactive)
   (delete-indentation t))
 
+(defun rename-current-buffer-file ()
+  "Renames the current buffer and the file it is visiting."
+  (interactive)
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " filename)))
+        (if (get-buffer new-name)
+            (error "A buffer named '%s' already exists!" new-name)
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil)
+          (message "File '%s' successfully renamed to '%s'"
+                   name (file-name-nondirectory new-name)))))))
+
 (defun delete-current-buffer-file ()
   "Delete the current buffer and the file connected with it"
   (interactive)
