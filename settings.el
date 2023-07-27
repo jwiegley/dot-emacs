767332c8d78d022aac9ccc1ffd1a4b9bc580356b
John Wiegley <johnw@newartisans.com>
2023-07-26 20:07:50 -0700

    changes

---
+++ settings.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-fold-catch-invisible-edits 'error nil nil "Customized with use-package org")
+ '(org-modern-star nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
