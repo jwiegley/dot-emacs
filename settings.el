96ebced833d142545e8833656580465dc358dc94
John Wiegley <johnw@newartisans.com>
2023-07-31 11:18:14 -0700

    changes

--- settings.el
+++ settings.el
@@ -3,11 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-fold-catch-invisible-edits 'error nil nil "Customized with use-package org")
- '(org-modern-star nil))
+ '(org-fold-catch-invisible-edits 'error nil nil "Customized with use-package org"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-extra-emphasis-01 ((t (:inherit org-extra-emphasis :background "yellow" :foreground "black")))))
