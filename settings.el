c1541114e593e551d9bac9332abd3710b1e6c11c
John Wiegley <johnw@newartisans.com>
2023-08-06 12:40:38 -0700

    changes

--- settings.el
+++ settings.el
@@ -3,10 +3,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-fold-catch-invisible-edits 'error nil nil "Customized with use-package org"))
+ '(org-fold-catch-invisible-edits 'error nil nil "Customized with use-package org")
+ '(org-tag-alist
+   '(("Call" . 99)
+     ("Errand" . 101)
+     ("Home" . 104)
+     ("Impact" . 73)
+     ("HOT" . 72)))
+ '(org-tag-faces
+   '(("Call" :foreground "Orange" :weight bold)
+     ("Impact" :foreground "Magenta" :weight bold))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-extra-emphasis-01 ((t (:inherit org-extra-emphasis :background "yellow" :foreground "black")))))
+ )
