719d2ce5237c3b0a7c2d373e733e6042a370546d
John Wiegley <johnw@newartisans.com>
2026-09-17 00:46:58 -0700

    changes

--- abbrevs.el
+++ abbrevs.el
@@ -1,28 +1,29 @@
-;; -*- coding: utf-8 -*-
-
+;;-*-coding: utf-8;-*-
 (define-abbrev-table 'coq-mode-abbrev-table
-  '(("refl" "Proof. reflexivity. Qed.")))
-
-(define-abbrev-table 'message-mode-abbrev-table
-  '(("linux" "GNU/Linux")))
+  '(
+    ("refl" "Proof. reflexivity. Qed." nil :count 0)
+   ))
 
 (define-abbrev-table 'global-abbrev-table
-  '(("Abdul" "‘Abdu’l-Bahá")
-    ("Allahu" "Alláh’u’Abhá")
-    ("Aqdas" "Kitáb-i-Aqdas")
-    ("Bahai" "Bahá’í")
-    ("Ridvan" "Riḍván")
-    ("Bahau" "Bahá’u’lláh")
-    ("Iqan" "Kitáb-i-Íqán")
-    ("Ridvan" "Riḍván")
-    ("Huquq" "Ḥuqúqu’lláh")
-    ("RBC" "Regional Bahá’í Council")
-    ("LSA" "Local Spiritual Assembly")
-    ("NSA" "National Spiritual Assembly")
-    ("UHJ" "Universal House of Justice")))
+  '(
+    ("Abdul" "‘Abdu’l-Bahá" nil :count 0)
+    ("Allahu" "Alláh’u’Abhá" nil :count 0)
+    ("Aqdas" "Kitáb-i-Aqdas" nil :count 0)
+    ("Bahai" "Bahá’í" nil :count 5)
+    ("Bahau" "Bahá’u’lláh" nil :count 0)
+    ("Huquq" "Ḥuqúqu’lláh" nil :count 2)
+    ("ITC" "International Teaching Centre" nil :count 3)
+    ("Iqan" "Kitáb-i-Íqán" nil :count 0)
+    ("LSA" "Local Spiritual Assembly" nil :count 1)
+    ("NSA" "National Spiritual Assembly" nil :count 1)
+    ("RBC" "Regional Bahá’í Council" nil :count 1)
+    ("Ridvan" "Riḍván" nil :count 1)
+    ("UHJ" "Universal House of Justice" nil :count 2)
+    ("rti" "Regional Training Institute" nil :count 0)
+   ))
 
-;; Local Variables:
-;; after-save-hook: (check-parens quietly-read-abbrev-file)
-;; End:
+(define-abbrev-table 'message-mode-abbrev-table
+  '(
+    ("linux" "GNU/Linux" nil :count 0)
+   ))
 
-;;; abbrevs.el ends here
